ebb5219aa83e60a8fe1be41bdb58acf7e3fc9739
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-02 17:21:21 +0530

    Tables Of given Numbers.c

---
+++ 01 Tables Of given Numbers.c.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+  int Cnt = 1,No = 0;
+  
+   printf("Enter a Number To Print its Table =");
+   scanf("%d",&No);
+   
+   printf("\n Table of &d is \n",No);
+   
+   while(Cnt <= 10)
+   {
+     printf("\n %d",(No * Cnt));
+     
+     Cnt++;
+   }
+   getch();
+   return 0;
+}
