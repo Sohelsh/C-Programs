fdeb0562d8ffbc14d87f6210ce5b066ecd23f14e
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-05 23:53:21 +0530

    Program To Check Given Character Is Upper Case Or Lover Case Or Digit or Special Symbol

---
+++ Program To Check Given Character Is Upper Case Or Lover Case Or Digit or Special Symbol.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  char ch = '\0';
+  
+  printf("\n\nEnter a Character To Check Whether,\n\n(It is Upper case =  Or Lover Case = Or Digit = Or Special Symbol = )= ");
+  scanf("%c",&ch); // or ch = getche();
+  
+     if((ch >= 'A') && (ch <= 'Z'))
+      {
+        printf("\n\n Given Character is Uppercase Letter. ");
+      }
+      else if((ch >= 'a') && (ch <= 'z'))
+      {
+        printf("\n\n Given Character is Lovercase Letter");
+      }
+      else if((ch >='0') && (ch <= '9'))
+      {
+        printf("\n\n Given Character is Digit ");
+      }
+      else
+      {
+        printf("\n\n Given Character is Special Symbol ");
+      }
+      
+      printf("\n\n Thanks..");
+      
+      getch();
+      return 0;
+}
