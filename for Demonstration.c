c22e8a735cad679624182dc0230526be851d56bf
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-05 19:16:17 +0530

    for Demonstration

---
+++ for Demonstration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+   int i = 0;
+   
+   for(i = 1; i <= 5 ; i++)
+   {
+        printf("\n INDIA");
+        printf("\n I Love My INDIA ");
+   }
+   
+   printf("\n Thanks !!!");
+   
+   getch();
+   return 0;
+}
