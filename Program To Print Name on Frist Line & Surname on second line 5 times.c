e669acb02b3ab5eda014bfaa3565383d788d7cee
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-06 11:48:53 +0530

    Program To Print Name on Frist Line & Surname on second line 5 times

---
+++ Program To Print Name on Frist Line & Surname on second line 5 times.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+   int Cnt = 0;
+   
+   for(Cnt = 1; Cnt <= 5 ; Cnt++)
+   {
+     printf("Fork\n Infosystems \n\n");
+     
+   }
+   getche();
+   
+   printf("\n\n Thanks ");
+   
+   getch();
+   return 0;
+}
