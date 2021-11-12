edda6011072975e1bc051de4b74afc33c27c7612
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-12 15:58:22 +0530

    Add files via upload

---
+++ Basic Program/Size of.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("\n===================****************==================");
+
+  printf("\n Size for Integer Data type = %d",sizeof(int));
+  
+  printf("\n size for short int Data type =%d ",sizeof(short int));
+  
+  printf("\n Size for float Data type = %d",sizeof(float));
+  
+  printf("\n Size for char Data type = %d",sizeof(char));
+  
+  printf("\n Size for double Data type = %d",sizeof(double));
+  
+  printf("\n===================****************==================");
+  
+  return 0;
+  getch();
+} 
