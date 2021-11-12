4121191bdd6e0f13fdd806f9cdb777455d65c34f
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-12 21:23:35 +0530

    Add files via upload

---
+++ Digit Programs/21B)Program To Printf Tables Range SP To EP, EP To SP.c.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+  int R=0, C=0, SP=0, EP=0;
+  
+   printf("\n Enter Start Point For tables = ");
+   scanf("%d",&SP);
+   
+   printf("\n Enter End Point For Tables = ");
+   scanf("%d",&EP);
+   
+    printf("\n**********=====**********\n");
+      for(R=1; R <=10; R++)
+      {
+        if(SP < EP)
+        {
+             for(C = SP ; C <= EP; C++)
+            {
+              printf(" %3d ",R*C);
+            }
+        }
+        else
+        {
+           for(C = SP ;C >= EP ; C--)
+           {
+             printf(" %3d ",R*C);
+           }
+           
+        }
+        printf("\n");
+       }
+        printf("\n***********=====**********");
+         
+      getch();
+      return 0;
+}
