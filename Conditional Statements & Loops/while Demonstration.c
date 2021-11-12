108e0a8f396e8c438064f3ca4b84ccd8967f9a49
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-12 21:39:00 +0530

    Add files via upload

---
+++ Conditional Statements & Loops/while Demonstration.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int i = 0;
+  
+  while( i <= 5)
+  {
+    printf("\n INDIA");
+    printf("\n I Love My INDIA");
+    i++;
+  }
+  
+  getch();
+  return 0;
+}
