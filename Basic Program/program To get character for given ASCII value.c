3487e0acb670ac641ba0752e73abce58007c9122
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-12 15:57:56 +0530

    Add files via upload

---
+++ Basic Program/program To get character for given ASCII value.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+     int Val = 0;
+     
+     printf("\n Enter a character = ");
+     scanf("%d",&Val);
+     
+     
+     printf("\n\n Character for ASCII Value  %d is = %c .", Val,Val);
+ 
+ 
+ 
+      getch();
+      return 0;
+} 
