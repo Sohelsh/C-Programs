337a7f13bd4d57f3095b21a33ff0835517b8a8a7
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-13 18:52:47 +0530

    Add files via upload

---
+++ Basic Program/Formatted Output.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  printf("\n %-5d %-20s %-3d %3d %3d %3f",255,"Amit",9,81,150,50.00);
+  
+  printf("\n %-5d %-20s %-3d %3d %3d %3f",150,"Sujata",70,80,200,66.67);
+  
+  printf("\n %-5d %-20s %-3d %3d %3d %3f",33,"Ajay",82,18,170,56.67);
+  return 0;
+  getch();
+}
