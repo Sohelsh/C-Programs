15213f92519fc203a9f070f1ec79be091942ae4c
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-13 19:22:01 +0530

    Add files via upload

---
+++ Basic Program/4B)Printf Demo 2.c
@@ -0,0 +1,22 @@
+// Addition of 2  Numbers 
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{ 
+  int No1= 25,No2 = 75, sum = 0;
+  
+  printf("\n we are having addition now!");
+  
+  printf("\n\n Before Performing Addition -> sum = %d.",sum);
+
+  sum = No1 + No2;
+  
+  getch();
+  
+  printf("\n\n Addition of %d & %d = %d.",No2, No1, sum);
+
+  getch();
+  return 0; 
+} 
