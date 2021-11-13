47e59326c010d64d774f88e3d61bc26d80fd656d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-13 19:13:52 +0530

    Add files via upload

---
+++ Practice Program/Voter is Eligible Or Not Eligible ( Use if else).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Age = 0;
+  
+  printf("\n Enter Voter Age = ");
+  scanf("%d",&Age);
+  
+  if(Age >= 18)
+   {
+     printf("\n Voter is Eligible ");
+   }
+   else
+   {
+     printf("\n Voter is Not Eligible ");
+   }
+    
+  getch();
+  return 0;
+}
