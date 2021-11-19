5b16148ac0c42e289206e8566f3a275cf372c07b
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-19 17:15:07 +0530

    Add files via upload

---
+++ Array Programs/Basic Program/01. Array.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  int Values[5] = {25,99,45,50,65};
+  
+  printf("\n Value of 1st Elements Number = %d",Values[0]);
+  printf("\n Value of 2nd Elements Number = %d",Values[1]);
+  printf("\n Value Of 3nd Elements Number = %d",Values[2]);
+  printf("\n Value of 4th Elements Number = %d",Values[3]);
+  printf("\n Value of 5th Elements Number = %d",Values[4]);
+  
+  getch();
+  return 0;
+}
