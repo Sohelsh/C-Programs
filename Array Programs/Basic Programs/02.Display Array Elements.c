a16d4fd60d6b3c4912847cb37be7498c309525a3
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2021-11-19 17:05:27 +0530

    Add files via upload

---
+++ Array Programs/Basic Programs/02.Display Array Elements.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  int Values[5] ={};
+  
+  printf("\n Value of 1st Elements Number = %d",Values[0]);
+  printf("\n Value of 2nd Elements Number = %d",Values[1]);
+  printf("\n Value Of 3nd Elements Number = %d",Values[2]);
+  printf("\n Value of 4th Elements Number = %d",Values[3]);
+  printf("\n Value of 5th Elements Number = %d",Values[4]);
+  
+  
+  getch ();
+  return 0;
+}
