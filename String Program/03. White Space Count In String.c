08d099b1ad42be8ec21d082931ee3e32d60b34ac
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-04 22:30:25 +0530

    Add files via upload

---
+++ String Program/03. White Space Count In String.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+  char cSrc[30]={'\0'};
+  int i =0,scout =0;
+  
+  printf("Enter A String = ");
+  gets(cSrc);
+  
+     while(cSrc[i] != '\0')
+     {
+        if(cSrc[i] == ' ')
+       {
+          scout++;
+       }
+        i++;
+     }
+ 
+   
+     printf("\n Space Count is %d",scout);
+   
+  getch();
+  return 0;
+}
