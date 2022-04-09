eb2c4187ba8e9a4e4d6682aa4c84c8174c4354b8
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-09 19:32:28 +0530

    Add files via upload

---
+++ Fork Assignment/01. Assignment.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+//Count Of Negative Numbers
+
+int main()
+{
+  int Num[15] ={'\0'};
+  int i = 0 ,N_Cnt = 0;
+  
+  printf("Enter A Number = ");
+  gets(Num);
+  
+  while(Num[i] != '\0')
+  {
+     if(Num[i] <'0')
+     {
+       N_Cnt++;
+     }  
+     
+     i++;     
+  }
+   
+  printf("Given Numbers Negative Count Is = %d",N_Cnt);
+  
+  getch();
+  return 0;
+}
