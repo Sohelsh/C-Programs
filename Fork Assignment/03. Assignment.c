8a713be341e6a93c3f3ed18122a87e846e9e1267
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-09 19:33:20 +0530

    Add files via upload

---
+++ Fork Assignment/03. Assignment.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+// Count Of Zero 
+int main()
+{
+  int Num[15] ={'\0'};
+  int i = 0 ,Z_Cnt = 0;
+  
+  printf("Enter A Number = ");
+  gets(Num);
+  
+  while(Num[i] != '\0')
+  {
+     if(Num[i] == '0')
+     {
+       Z_Cnt++;
+     }  
+     
+     i++;     
+  }
+   
+  printf("Given Number Zero Count Is = %d",Z_Cnt);
+  
+  getch();
+  return 0;
+}
