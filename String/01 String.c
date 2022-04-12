a4f11aaf37523ab376155ac082c8894de591a057
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-12 23:02:17 +0530

    Add files via upload

---
+++ String/01 String.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <conio.h>
+//Compiler version gcc  6.3.0
+
+int main()
+{
+   char Schar[5]={'\0'};
+ 
+   puts("Enter a string =");
+   gets(Schar);
+   
+   puts("Given String Is =");
+   puts(Schar);
+  
+   
+   getch();
+   return 0;
+}
