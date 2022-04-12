f0e55d1e45227efc198af1224c72c2d62e8f40bf
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-12 23:04:10 +0530

    Add files via upload

---
+++ String/06.Count of Digit In String.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  char cSrc[40]={'\0'};
+  int i = 0,D_cnt =0;
+  
+  printf("Enter A String = ");
+  gets(cSrc);
+  
+  while(cSrc[i] != '\0')
+  {
+    if(cSrc[i] >= '0'&& cSrc[i] <= '9')
+    {
+      D_cnt++;
+    }
+    i++;
+  }
+   printf("\n Count Of Digits In String : %d",D_cnt);
+  
+  
+  getch();
+  return 0;
+  
+}
