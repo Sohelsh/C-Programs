5d501466e6cff0fcacefacc3c02bdda780e72d81
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-13 23:00:49 +0530

    Add files via upload

---
+++ Pattern Program/04.New Pattern Printing Right Lower.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+
+    int R = 0,C = 0 ,N =0;
+
+    printf("\n Enter Row and Column Size =");
+    scanf("%d",&N);
+    
+    for(R = 1;R <= N ; R++)
+    {
+      for(C =1;C <= N ; C++)
+      {
+        if(R+C > N)
+        {
+          printf(" * ");
+        }
+        else
+        {
+          printf("   ");
+        }
+        
+      }
+      printf("\n");
+    }
+    
+    
+    getch();
+    return 0;
+ }
