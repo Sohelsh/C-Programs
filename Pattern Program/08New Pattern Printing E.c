75f1d76f7d7c060b1bf8ad15014bcf3118bd0f12
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-04-18 23:24:25 +0530

    Add files via upload

---
+++ Pattern Program/08New Pattern Printing E.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <conio.h>
+
+//Compiler version gcc  6.3.0
+
+int main()
+{
+   int R = 0,C = 0 ,N =0;
+   
+   printf("\n Entar Pattern Size =");
+   scanf("%d",&N);
+  
+  for(R =1 ;R <= N; R++)
+   {
+     for(C = 1; C<= N; C++)
+     {
+       if(R == 1 || R == N ||C == 1 || R == (N/2)+1&& R>=C)
+       {
+         printf(" * ");
+       }
+       else
+        {
+          printf("   ");
+        }
+     }
+     printf("\n");
+   }
+  }
