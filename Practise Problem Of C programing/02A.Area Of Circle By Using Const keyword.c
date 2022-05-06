6df0a445fe897eca5b58975289724003c3906c89
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-05-07 00:13:47 +0530

    Area Of Circal Without Define

---
+++ Practise Problem Of C programing/02A.Area Of Circle By Using Const keyword.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <conio.h>
+//Compiler version gcc  6.3.0
+
+int main()
+{
+  const pi = 3.14;
+  int R = 0;
+  float Area = 0.0;
+  
+  printf("\n Enter A Radious = ");
+  scanf("%d",&R);
+  
+  Area = pi * R * R;
+  
+  printf("\n Area Of Circal =%f",Area);
+  
+  getch();
+  return 0;
+}
