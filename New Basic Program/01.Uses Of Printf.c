7e422efde19d0b60d9ba5532a8c3ac2d5a41237f
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-05-30 23:30:09 +0530

    Uses Of Printf

---
+++ New Basic Program/01.Uses Of Printf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    printf("\n %-5d %-10s %-3d %3d %d %3f",255,"Amit",07,81,150,50.00);
+
+    printf("\n %-5d %-10s %-3d %3d %d %3f",02,"Raj",70,80,200,67.55);
+
+    printf("\n %-5d %-10s %-3d %3d %d %3f",150,"Ajay",82,18,170,56.40);
+
+
+
+    getch();
+    return 0;
+}
