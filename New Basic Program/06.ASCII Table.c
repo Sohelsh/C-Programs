bd560c843b08eaba322e7075d3f3d9bcfbd11175
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-01 23:50:36 +0530

    ASCII Table (0-127)

---
+++ New Basic Program/06.ASCII Table.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>>
+
+int main()
+{
+    int No=0;
+
+    for(No =0 ;No < 128;No++)
+
+
+    printf("\n ASCII Table %d = %c",No,No);
+
+
+    getch();
+    return 0;
+}
