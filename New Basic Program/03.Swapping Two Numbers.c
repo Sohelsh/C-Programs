0daf18047ab208b3178a565de0ed6c1fffffd5a1
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-01 23:45:58 +0530

    Swapping Two Number

---
+++ New Basic Program/03.Swapping Two Numbers.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1=0,No2=0, Temp=0;
+
+    printf("\n Enter Two Number =  ");
+    scanf("%d%d",&No1,&No2);
+
+    printf("\n\n Before Swap No1 = %d,No2 = %d",No1,No2);
+
+    Temp = No1;
+    No1 = No2;
+    No2 = Temp;
+
+
+    printf("\n\n After Swap No1 = %d,No2 = %d",No1,No2);
+
+    getch();
+    return 0;
+
+
+
+
+}
