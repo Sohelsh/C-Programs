81a642d592a5db4f711e17096eb41dcb4ca56356
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-01 23:49:49 +0530

    Uses Of Sizeof Opreter

---
+++ New Basic Program/05. Use Of Size Of Operater.c
@@ -0,0 +1,16 @@
+#include<stdio.h>>
+#include<conio.h>
+
+int main()
+{
+    printf("\n Size Of Int  =   %d",sizeof(int));
+
+    printf("\n Size Of Float =  %d",sizeof(float));
+
+    printf("\n Size Of Double = %d",sizeof(double));
+
+    printf("\n Size Of Char =   %d",sizeof(char));
+
+    getch();
+    return 0;
+}
