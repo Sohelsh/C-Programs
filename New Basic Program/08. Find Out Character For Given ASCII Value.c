73a64b52b55ab15e5e002379c1974e9125315857
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-02 22:57:12 +0530

    Find Out Character For Given ASCII Value

---
+++ New Basic Program/08. Find Out Character For Given ASCII Value.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No=0;
+
+
+    printf("\n ASCII to Character Conversion  \n");
+
+    printf("\n Enter A character = ");
+    scanf("%d",&No);
+
+    printf("\n ASCII value Of Charcater %d= %c",No,No);
+
+
+    getch();
+    return 0;
+}
