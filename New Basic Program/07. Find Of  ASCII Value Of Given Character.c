ca07715cf9dc71a38da98a5304250ce61cda3152
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-02 22:56:26 +0530

    Find Of ASCII Value Of Given Character

---
+++ New Basic Program/07. Find Of  ASCII Value Of Given Character.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Ch=0;
+
+
+    printf("\n Character to ASCII Conversion  \n");
+
+    printf("\n Enter A character = ");
+    scanf("%c",&Ch);
+
+
+
+    printf("\n ASCII value Of Charcater %c = %d",Ch,Ch);
+
+
+    getch();
+    return 0;
+}
