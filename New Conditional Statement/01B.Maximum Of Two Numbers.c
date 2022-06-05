7856e648e73699a378bbdc7c7d881098f817de4d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-05 23:08:59 +0530

    Program Of Maximum Of Two Numbar Using Of goto

---
+++ New Conditional Statement/01B.Maximum Of Two Numbers.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<conio.h>
+
+                                    // Use Of goto keyword
+ int main()
+ {
+     int No1 =0, No2 =0;
+
+     up:
+     printf("\n Enter A Numbar = ");
+     scanf("%d%d",&No1,&No2);
+
+
+     if(No1 == No2)
+     {
+         printf("\n\n  Both Number Is Equal ");
+         goto up;
+     }
+
+     else if(No1 > No2)
+     {
+         printf("\n First Numbar Is Maximum ");
+     }
+     else
+     {
+         printf("\n  Second Numbar Is Maximum");
+     }
+
+
+     getch();
+     return 0;
+ }
