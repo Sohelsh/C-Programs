4603f5f410e16c33c3293c3dd4304df5f35db691
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-05 23:07:24 +0530

    Program Of Maximum Of Two Numbers

---
+++ New Conditional Statement/01A.Maximum Of Two Numbers.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+ int main()
+ {
+     int No1 =0, No2 =0;
+
+     printf("\n Enter A Numbar = ");
+     scanf("%d%d",&No1,&No2);
+
+     if(No1 > No2)
+     {
+         printf("\n First Numbar Is Maximum ");
+     }
+     else
+     {
+         printf("\n  Second Numbar Is Maximum");
+     }
+
+
+     getch();
+     return 0;
+ }
