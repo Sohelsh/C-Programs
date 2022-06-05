5630aa95ad5b0c21f6533c5a454d63ac66a80221
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-05 23:09:51 +0530

    Program Maximum Of Two Numbers Using Ternary Operator

---
+++ New Conditional Statement/02.Maximum Of Two Numbers Using Ternary Operator.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int No1 =0, No2 =0;
+
+    printf("\n Enter A Numbars = ");
+    scanf("%d%d",&No1,&No2);
+
+    (No1 > No2)?printf("\n First Numbar is Maximum "):printf("\n Second Numbar Is MAximum ");
+
+    getch();
+    return 0;
+}
