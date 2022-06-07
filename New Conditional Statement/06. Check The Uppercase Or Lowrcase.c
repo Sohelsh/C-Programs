ac654d804ec6af041501a0d5c1755aa3565bdcf1
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-07 23:40:47 +0530

    Program Of Check The Uppercase Or Lowrcase

---
+++ New Conditional Statement/06. Check The Uppercase Or Lowrcase.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Ch= '\0';
+
+    printf("\n Entar A Charactor = ");
+    Ch =getche(); //scanf("%c",&Ch);
+
+    if(Ch >= 'A' && Ch <= 'Z')
+    {
+        printf("\n Given Charactor Is Uppercase = %c ",Ch);
+    }
+    else if(Ch >= 'a' && Ch <= 'z')
+    {
+        printf("\n Given Charactor Is Lowercase = %c ",Ch);
+    }
+    else
+    {
+         printf("\n Given Charactor Is Naither Lowercase and Upparcase = %c ",Ch);
+    }
+
+
+    getch();
+    return 0;
+}
