3e56084f9eb979d57de3bda792fcc1d09a38b3b6
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-09 21:46:38 +0530

    Program Of Sum Of Digit Of Inputted 3 Digit Numbar

---
+++ New Digit Program/01.Sum Of Digit Of Inputted 3 Digit Numbar.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+ int main()
+ {
+     int No=0,Sum=0,Dig =0,Temp=0;
+
+     printf("\n Entar  3 Digit Numbar =  ");
+     scanf("%d",&No);
+
+     Temp=No;
+
+     while(Temp > 0)
+     {
+
+        Dig  = Temp %10;
+        Temp = Temp / 10;
+        Sum  = Sum + Dig;
+
+
+     }
+      printf("%d",Sum);
+
+      getch();
+      return 0;
+ }
