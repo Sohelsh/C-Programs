86344d17adce62628f113418969ac50c86cfbbc9
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-23 00:28:26 +0530

    Add files via upload

---
+++ Practise Problem Of C programing/01.Structure.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<conio.h>
+ struct Workshop
+ {
+     char Std_Name[8];
+     char Std_Cours[8];
+     int Std_Fee;
+ };
+ int main()
+ {    int i =0;
+      struct Workshop wor[3];
+
+     for(i = 1;i <= 3 ;i++)
+     {
+         printf("\n Entar Student Name = ");
+         scanf("%s",&wor[i].Std_Name);
+
+         printf("\n Entar Student Cours = ");
+         scanf("%s",&wor[i].Std_Cours);
+
+         printf("\n Entar Student Fee = ");
+         scanf("%d",&wor[i].Std_Fee);
+     }
+    for(i = 1;i < 3 ;i++)
+    {
+           printf("\n first Student information =%s %s %d",wor[i].Std_Name,wor[i].Std_Cours,wor[i].Std_Fee);
+
+    }
+
+
+ }
+
+
+
+
+
