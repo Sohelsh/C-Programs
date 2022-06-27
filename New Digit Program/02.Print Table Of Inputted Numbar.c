689b1cc1fac57b0f876ef5b2b0da1178363b84ed
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-27 23:28:46 +0530

    Print Table Of Inputted Numbar

---
+++ New Digit Program/02.Print Table Of Inputted Numbar.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+    int Cnt = 0,No = 0;
+
+    printf("\n Entar A Numbar = ");
+    scanf("%d",&No);
+
+    for(Cnt = 1; Cnt <= 10; Cnt++)
+    {
+         printf("%d",Cnt * No);
+
+         printf("\n");
+    }
+      return 0;
+      getch();
+ }
