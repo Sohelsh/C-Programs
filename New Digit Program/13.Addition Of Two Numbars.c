60a323cf6b11e88a02159589fd30f05fdce595a9
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-06-28 15:26:50 +0530

    Add files via upload

---
+++ New Digit Program/13.Addition Of Two Numbars.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No1 = 0,No2 =0,Add = 0;
+
+      printf("\n Entar Two Numbars Of Addition = ");
+      scanf("%d%d",&No1,&No2);
+
+      Add = No1 + No2;
+
+      printf("\n Addition  Of |%d| + |%d| = %d",No1,No2,Add);
+
+      getch();
+      return 0;
+
+ }
+
---
+++ New Digit Program/14.Percentage Of Given Numbers.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No1 = 0,No2 =0,Per = 0;
+
+      printf("\n Entar A Number Of Percentage = ");
+      scanf("%d",&No1);
+
+      printf("\n How Much Percent = ");
+      scanf("%d",&No2);
+
+      Per = (No1 * No2) /100;
+
+      printf("\n Addition  Of |%d| & |%d| = %d",No1,No2,Per);
+
+      getch();
+      return 0;
+
+ }
+
+
