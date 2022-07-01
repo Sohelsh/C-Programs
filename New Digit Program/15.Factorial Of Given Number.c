6d03ee5dfd02db18b20dcad79e4ccd5505aa91f7
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-01 22:47:47 +0530

    Factorial Of Given Number

---
+++ New Digit Program/15.Factorial Of Given Number.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No = 0,Fact = 1;
+
+      printf("\n Entar A Number = ");
+      scanf("%d",&No);
+
+      while(No != 0)
+      {
+           Fact = Fact * No;
+
+           No--;
+      }
+
+      printf("\n Factorial Of Number Is = %d",Fact);
+
+      getch();
+      return 0;
+ }
