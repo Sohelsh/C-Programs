af94fff9b10a6f4eca350c1ed480af00c5034df2
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-03 23:26:07 +0530

    Add files via upload

---
+++ Practise Problem Of C programing/03.Addtion Of Two Numbers Using Function.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+ int Add(int ,int);
+
+ int main()
+ {
+      int No1 = 0, No2 = 0;
+
+      printf("\n Entar Two Numbers = ");
+      scanf("%d%d",&No1,&No2);
+
+      Add(No1,No2);
+
+
+ }
+ int Add(int N1,int N2)
+ {
+      int Sum = 0;
+
+      Sum = N1 + N2;
+
+      printf("%d",Sum);
+ }
