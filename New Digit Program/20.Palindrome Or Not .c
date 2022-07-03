5f1cd58cb3837b4ad6473f0b643025ee46abc5f6
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-03 23:19:13 +0530

    Palindrome Or Not

---
+++ New Digit Program/20.Palindrome Or Not .c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+     int No = 0,Temp = 0,Rev = 0,Dig = 0;
+
+     printf("\n Entar A Number = ");
+     scanf("%d",&No);
+
+     Temp=No;
+
+     while(Temp != 0)
+     {
+          Dig  = Temp %10;
+          Temp = Temp /10;
+          Rev  = (Rev*10)+Dig;
+     }
+
+      if(Rev == No)
+      {
+           printf("\n Given Number %d Is Palindrome ",No);
+      }
+      else
+      {
+           printf("\n Given Number %d Is Not Palindrome ",No);
+      }
+
+      getch();
+      return 0;
+}
