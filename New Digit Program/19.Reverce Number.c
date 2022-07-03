8bf6e61761d8e4a708f15098fbdbfa54ad4597c4
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-03 23:18:38 +0530

    Reverce Number

---
+++ New Digit Program/19.Reverce Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No = 0,Temp = 0,Rev = 0,Dig = 0;
+
+      printf("\n Entar A Number = ");
+      scanf("%d",&No);
+
+      Temp=No;
+
+      while(Temp != 0)
+      {
+           Dig = Temp % 10;
+           Temp = Temp /10;
+           Rev = (Rev*10) +Dig;
+      }
+
+       printf("\n Reverce Of Given Number %d = %d",No,Rev);
+
+
+       getch();
+       return 0;
+ }
