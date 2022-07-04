4714a83669325036dfcceb64e2096820dfb0adba
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-04 23:19:03 +0530

    Add files via upload

---
+++ New Digit Program/21.Fibonaci Seris .c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int N1 = 1,N2 = 0,N3 = 0,Cnt = 0;
+
+      printf("Entar A Number = ");
+      scanf("%d",&Cnt);
+
+      while(Cnt != 0)
+      {
+           printf(" %d ",N3);
+
+           N3 = N1 + N2;
+           N1 = N2;
+           N2 = N3;
+
+           Cnt--;
+      }
+
+       getch();
+ }
