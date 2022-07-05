8cdea79d85cefdec0cd69b4225947d27a2961c09
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-05 23:25:00 +0530

    Add files via upload

---
+++ New Conditional Statement/11.Program Of While Use.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+     ///Program To Print "Fork" On first Line & "INFOSYSTEMS" On Second Line Print This 5 Times
+
+ int main()
+ {
+      int i = 5;
+
+      while(i > 0)
+      {
+           printf("\nFork");
+           printf("\n INFOSYSTEMS");
+
+           i--;
+      }
+      getch();
+      return 0;
+
+ }
