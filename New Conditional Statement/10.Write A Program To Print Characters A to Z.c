412ffd996e5cb921936981ee8048830958102108
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-05 23:24:11 +0530

    Program To Print Characters A to Z.

---
+++ New Conditional Statement/10.Write A Program To Print Characters A to Z.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     char Ch = '\0';
+
+     for(Ch = 'A';Ch <= 'Z';Ch++)
+     {
+          printf(" %c ",Ch);
+     }
+
+     getch();
+     return 0;
+ }
