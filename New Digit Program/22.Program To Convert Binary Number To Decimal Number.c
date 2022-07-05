9e666ac88c15f5d4d0f6aac7299f1e3b3a6d49db
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-05 23:21:39 +0530

    Program To Convert Binary Number To Decimal Number

---
+++ New Digit Program/22.Program To Convert Binary Number To Decimal Number.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No = 0,Temp = 0,Dig = 0, Dec = 0,Base =1;
+
+      printf("\n Entar Binary Number = ");
+      scanf("%d",&No);
+
+      Temp = No;
+
+      while(Temp != 0)
+      {
+           Dig = Temp %10;
+           Temp= Temp /10;
+           Dec = Dec + Dig *Base;
+           Base = Base *2;
+      }
+
+      printf("\n Binary Number = %d To Decimal Number = %d",No,Dec);
+
+      getch();
+      return 0;
+ }
