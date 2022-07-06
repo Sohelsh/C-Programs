fe3040efab421138552ea601229cbe5e522c8ecf
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-06 23:42:17 +0530

    Add files via upload

---
+++ Practise Problem Of C programing/14.Program Of Print Only Last Digit Of Given Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+      int No = 0,Dig = 0;
+
+      printf("\n Entar A Number = ");
+      scanf("%d",&No);
+
+      Dig = No %10;
+
+      printf("\n Given Number Last Digit Is : %d",Dig);
+
+      getch();
+      return 0;
+ }
---
+++ Practise Problem Of C programing/15.Fibonaoci Seris Using Function.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int Fibonaoci(int);
+ int main()
+ {
+      int Cnt  = 0;
+
+      printf("\n Entar A Count For Series = ");
+      scanf("%d",&Cnt);
+
+       Fibonaoci(Cnt);
+
+      getch();
+ }
+  int Fibonaoci(Count)
+  {
+       int N1 = 1,N2 = 0,N3 = 0;
+
+       while(Count != 0)
+       {
+          printf(" %d ",N3);
+
+          N3 = N1 + N2;
+          N1 = N2;
+          N2 = N3;
+
+          Count--;
+       }
+        return N3;
+  }
