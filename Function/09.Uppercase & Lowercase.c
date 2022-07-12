52757be501d39916211db96e0143eba60aab0b1e
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-12 20:42:45 +0530

    Add files via upload

---
+++ Function/09.Uppercase & Lowercase.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<conio.h>
+
+ char Uppercase_And_Lowercase(char);
+
+ int main()
+ {
+          char Ch = '\0';
+
+          printf("\n Entar Character = ");
+          scanf("%c",&Ch);
+
+          Uppercase_And_Lowercase(Ch);     //Functoin Call
+
+ }
+ char Uppercase_And_Lowercase(char C)
+ {
+     if(C >= 'A' && C <='Z')
+     {
+          printf("\n***Given Character Is Uppercase***");
+
+          getch();
+
+          printf("\n Uppercase Series Is: ");
+
+          for(C = 'A';C <= 'Z';C++)
+          {
+              printf(" %c ",C);
+          }
+
+     }
+     else if(C >= 'a' && C <= 'z')
+     {
+          printf("\n Given Character Is Lowercase ");
+
+          getch();
+
+          printf("\n Lowercase Series Is: ");
+
+          for(C = 'a';C <= 'z';C++)
+          {
+              printf(" %c ",C);
+          }
+
+     }
+     else
+     {
+          printf("\n Invild Input....");
+     }
+
+          getch();
+          return;
+ }
+
