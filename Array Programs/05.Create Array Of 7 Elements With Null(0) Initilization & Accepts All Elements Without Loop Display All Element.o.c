58078ba796068d9ea8431aa48d4422675dbe0508
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-14 20:34:10 +0530

    Add files via upload

---
+++ Array Programs/05.Create Array Of 7 Elements With Null(0) Initilization & Accepts All Elements Without Loop Display All Element.o.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     int i = 0, Arr[7]={0};
+
+      for(i = 0; i < 7 ; i++)
+      {
+         printf("\n Entar A Elements No %d = ",i + 101);
+         scanf("%d",&Arr[i]);
+      }
+
+      for(i = 0; i < 7 ; i++)
+      {
+          printf("\n Value Of %d Elements = %d ",i+101,Arr[i]);
+      }
+
+      getch();
+      return 0;
+ }
