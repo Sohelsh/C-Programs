7d005f8d8ced0d3255267ccde3d536ad53a45247
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-14 20:30:04 +0530

    Add files via upload

---
+++ Array Programs/02.Create Array With Initilization & Display All Elements.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     int Value[5]={23,53,64,58,26};
+
+        printf("\n Frist  Element = %d",Value[0]);
+        printf("\n Second Element = %d",Value[1]);
+        printf("\n Third  Element = %d",Value[2]);
+        printf("\n Fourth Element = %d",Value[3]);
+        printf("\n Fifth  Element = %d",Value[4]);
+
+        getch();
+ }
