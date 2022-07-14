dd836c91e5998e9eb3cfa22905f319df0f45fbd0
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-14 20:34:30 +0530

    Add files via upload

---
+++ Array Programs/03.Create Array Of 7 Elements With Null(0) Initilization & Display All Elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     int Value[7]={0};
+
+        printf("\n Frist   Element = %d",Value[0]);
+        printf("\n Second  Element = %d",Value[1]);
+        printf("\n Third   Element = %d",Value[2]);
+        printf("\n Fourth  Element = %d",Value[3]);
+        printf("\n Fifth   Element = %d",Value[4]);
+        printf("\n Sixth   Element = %d",Value[5]);
+        printf("\n Sevnth  Element = %d",Value[6]);
+
+        getch();
+ }
