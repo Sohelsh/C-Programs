b66c25af461154ddf9e28fae0df720a279671877
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-15 22:56:18 +0530

    Add files via upload

---
+++ Array With Function/02.Create Array With Initilization & Display All Elements With Function.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int Arr_Display();
+
+ int main()
+ {
+        Array_Element();
+
+        printf("\n Thanks..........");
+ }
+
+ Array_Element()
+ {
+        int Arr[5]={65,76,28,27,12};
+
+        printf("\n Element Is = %d",Arr[0]);
+        printf("\n Element Is = %d",Arr[1]);
+        printf("\n Element Is = %d",Arr[2]);
+        printf("\n Element Is = %d",Arr[3]);
+        printf("\n Element Is = %d",Arr[4]);
+
+        getch();
+        return 0;
+
+ }
