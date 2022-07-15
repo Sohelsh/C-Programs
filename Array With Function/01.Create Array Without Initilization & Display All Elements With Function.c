e0e18c43fa73fd923bd75a03ab06234cf23e4043
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-15 22:56:04 +0530

    Add files via upload

---
+++ Array With Function/01.Create Array Without Initilization & Display All Elements With Function.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int Array_Element();
+
+ int main()
+ {
+        Array_Element();
+
+ }
+int Array_Element()
+{
+        int Arr[5]={0};
+
+        printf("\n Frist  Element = %d",Arr[0]);
+        printf("\n Second Element = %d",Arr[1]);
+        printf("\n Third  Element = %d",Arr[2]);
+        printf("\n Fourth Element = %d",Arr[3]);
+        printf("\n Fifth  Element = %d",Arr[4]);
+
+        return 0;
+
+}
+
+
+
