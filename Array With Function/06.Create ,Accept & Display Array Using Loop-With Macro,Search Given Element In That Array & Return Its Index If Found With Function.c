f4b6be6f14d4bb7a06fa60b15405dd4256c1d58d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-16 22:09:58 +0530

    Add files via upload

---
+++ Array With Function/06.Create ,Accept & Display Array Using Loop-With Macro,Search Given Element In That Array & Return Its Index If Found With Function.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+#define Cnt 3
+
+ int Accepts_Element(int N1[]);
+ int Display_Element(int N2[]);
+ int Search_Element(int N3[]);
+
+ int main()
+ {
+      int Arr[Cnt] = {0};
+
+      Accepts_Element(Arr);
+
+
+      getch();
+      system("cls");
+
+      Display_Element(Arr);
+
+      getch();
+      system("cls");
+
+      Search_Element(Arr);
+
+      printf("\n\n Thanks For Visiting .....");
+ }
+ int Accepts_Element(int N1[])
+ {
+      int i = 0;
+
+      printf("\n ********Entar A Elements*********");
+      for(i = 0;i < Cnt ;i++)
+      {
+            printf("\n Entar A Elements %d =  ",i+1);
+            scanf("%d",&N1[i]);
+      }
+
+      return;
+
+ }
+  int Display_Element(int N2[])
+  {
+       int i = 0;
+
+       printf("\n **********Elements List************");
+        for(i = 0;i < Cnt ;i++)
+      {
+            printf("\n Elements Is %d = %d ",i+1,N2[i]);
+      }
+       printf("\n**************************************");
+       return;
+  }
+   int Search_Element(int N3[])
+   {
+        int i = 0,Ser = 0;
+
+        printf("\n Entar A Search Element = ");
+        scanf("%d",&Ser);
+
+        for(i = 0;i < Cnt ;i++)
+        {
+             if(Ser == N3[i])
+             {
+                  break;
+             }
+        }
+        if(i < Cnt)
+        {
+             printf("\n Element Is Found ");
+        }
+        else
+        {
+             printf("\n Element Is Not Found ");
+        }
+
+        return;
+   }
