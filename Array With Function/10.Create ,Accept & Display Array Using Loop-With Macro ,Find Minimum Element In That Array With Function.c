08e377357d531df6b4f17a16a9ddb7988764db2b
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-16 22:14:16 +0530

    Add files via upload

---
+++ Array With Function/10.Create ,Accept & Display Array Using Loop-With Macro ,Find Minimum Element In That Array With Function.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<conio.h>
+#define Cnt 5
+
+ int Accepts_Element(int N1[]);
+ int Find_Minimum_Num(int N2[]);
+
+ int main()
+ {
+      int Arr[Cnt]={};
+
+      Accepts_Element(Arr);
+
+      getch;
+
+      printf("\n Minimum Number Is = %d ",Find_Minimum_Num(Arr));
+ }
+ int Accepts_Element(int N1[])
+ {
+      int i = 0;
+
+      for(i = 0;i < Cnt; i++)
+      {
+           printf("\n Entar Elements = ");
+           scanf("%d",&N1[i]);
+      }
+      return;
+ }
+ int Find_Minimum_Num(int N2[])
+ {
+      int i = 0,Min = 0;
+
+      for(i = 0;i < Cnt; i++)
+      {
+         if(Min > N2[i])
+         {
+              Min = N2[i];
+         }
+      }
+       return;
+ }
+
