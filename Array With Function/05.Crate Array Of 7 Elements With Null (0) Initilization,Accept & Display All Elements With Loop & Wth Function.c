5b30b68d581ee6b7770545e2ea9f2af874b6b815
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-16 22:09:40 +0530

    Add files via upload

---
+++ Array With Function/05.Crate Array Of 7 Elements With Null (0) Initilization,Accept & Display All Elements With Loop & Wth Function.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<conio.h>
+#define Cnt 3
+
+  int Accepts_Element(int Num[]);
+  int Display_Element(int No[] );
+
+  int main()
+  {
+       int Arr[Cnt] = {0};
+
+       Accepts_Element(Arr);
+
+       getch();
+
+       Display_Element(Arr);
+  }
+   int Accepts_Element(int Num[])
+   {
+        int i = 0;
+
+        for(i = 0;i < Cnt;i++)
+        {
+             printf("\n %d.Entar A Elements = ",i+101);
+             scanf("%d",&Num[i]);
+        }
+
+         return;
+   }
+   int Display_Element(int No[])
+   {
+        int i = 0;
+
+        for(i = 0;i < Cnt;i++)
+        {
+             printf("\n Elements Is = %d",No[i]);
+        }
+
+        return;
+
+   }
+
+
