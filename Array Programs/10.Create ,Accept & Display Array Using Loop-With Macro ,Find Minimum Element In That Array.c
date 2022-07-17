95305e67bba17299a141312b4db3f9bbafe26755
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-17 23:26:02 +0530

    Add files via upload

---
+++ Array Programs/10.Create ,Accept & Display Array Using Loop-With Macro ,Find Minimum Element In That Array.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+#define Cnt 8
+
+ int main()
+ {
+      int Arr[Cnt]={},i = 0,Min =0;
+
+      printf("\n\t Entar Eight Numbers = ");
+      for(i = 0;i < Cnt ;i++)
+      {
+           printf("\n Entar %d Number = ",i+1);
+           scanf("%d",&Arr[i]);
+      }
+      Min = Arr[0];
+      for(i = 0;i < Cnt;i++)
+      {
+           if(Min > Arr[i])
+           {
+                Min = Arr[i];
+           }
+      }
+      printf("\n Minimum Number Is = %d",Min);
+ }
