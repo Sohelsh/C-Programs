51eb66e0aef3c1a3637d000fab9b00c802059e35
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-18 22:57:47 +0530

    Update 06.Create ,Accept & Display Array Using Loop-With Macro,Search Given Element In That Array & Return Its Index If Found With Function.c

--- Array With Function/06.Create ,Accept & Display Array Using Loop-With Macro,Search Given Element In That Array & Return Its Index If Found With Function.c
+++ Array With Function/06.Create ,Accept & Display Array Using Loop-With Macro,Search Given Element In That Array & Return Its Index If Found With Function.c
@@ -8,71 +8,71 @@
 
  int main()
  {
-      int Arr[Cnt] = {0};
+           int Arr[Cnt] = {0};
 
-      Accepts_Element(Arr);
+           Accepts_Element(Arr);
 
 
-      getch();
-      system("cls");
+           getch();
+           system("cls");
 
-      Display_Element(Arr);
+           Display_Element(Arr);
 
-      getch();
-      system("cls");
+           getch();
+           system("cls");
 
-      Search_Element(Arr);
+           Search_Element(Arr);
 
-      printf("\n\n Thanks For Visiting .....");
+           printf("\n\n Thanks For Visiting .....");
  }
  int Accepts_Element(int N1[])
  {
-      int i = 0;
+           int i = 0;
 
-      printf("\n ********Entar A Elements*********");
-      for(i = 0;i < Cnt ;i++)
-      {
-            printf("\n Entar A Elements %d =  ",i+1);
-            scanf("%d",&N1[i]);
+           printf("\n ********Entar A Elements*********");
+           for(i = 0;i < Cnt ;i++)
+           {
+                 printf("\n Entar A Elements %d =  ",i+1);
+                 scanf("%d",&N1[i]);
+           }
+
+           return;
+
       }
-
-      return;
-
- }
   int Display_Element(int N2[])
   {
-       int i = 0;
+           int i = 0;
 
-       printf("\n **********Elements List************");
-        for(i = 0;i < Cnt ;i++)
-      {
-            printf("\n Elements Is %d = %d ",i+1,N2[i]);
-      }
-       printf("\n**************************************");
-       return;
+           printf("\n **********Elements List************");
+            for(i = 0;i < Cnt ;i++)
+          {
+                printf("\n Elements Is %d = %d ",i+1,N2[i]);
+          }
+           printf("\n**************************************");
+           return;
   }
    int Search_Element(int N3[])
    {
-        int i = 0,Ser = 0;
+           int i = 0,Ser = 0;
 
-        printf("\n Entar A Search Element = ");
-        scanf("%d",&Ser);
+           printf("\n Entar A Search Element = ");
+           scanf("%d",&Ser);
 
-        for(i = 0;i < Cnt ;i++)
-        {
-             if(Ser == N3[i])
-             {
-                  break;
-             }
-        }
-        if(i < Cnt)
-        {
-             printf("\n Element Is Found ");
-        }
-        else
-        {
-             printf("\n Element Is Not Found ");
-        }
+           for(i = 0;i < Cnt ;i++)
+           {
+                if(Ser == N3[i])
+                {
+                     break;
+                }
+           }
+           if(i < Cnt)
+           {
+                printf("\n Element Is Found ");
+           }
+           else
+           {
+                printf("\n Element Is Not Found ");
+           }
 
-        return;
+           return;
    }
