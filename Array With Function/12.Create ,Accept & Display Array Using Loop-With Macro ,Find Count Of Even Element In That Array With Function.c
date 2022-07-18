29829058196341432dec4597b4b5f95f01ee4c60
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-18 23:02:08 +0530

    Update 12.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even Element In That Array With Function.c

--- Array With Function/12.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even Element In That Array With Function.c
+++ Array With Function/12.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even Element In That Array With Function.c
@@ -7,38 +7,38 @@
 
  int main()
  {
-      int Arr[Cnt] ={};
+            int Arr[Cnt] ={};
 
-      Accepts_Element(Arr);
+            Accepts_Element(Arr);
 
-      getch();
+            getch();
 
-      printf("\n Count Of Even Elements  Is = %d",Find_Even_Cnt(Arr));
+            printf("\n Count Of Even Elements  Is = %d",Find_Even_Cnt(Arr));
 
  }
  int Accepts_Element(int N1[])
  {
-      int i =0;
+            int i =0;
 
-      for(i =0;i < Cnt;i++)
-      {
-           printf("\n Entar Numbers = ");
-           scanf("%d",&N1[i]);
-      }
-      return 0;
+            for(i =0;i < Cnt;i++)
+            {
+                 printf("\n Entar Numbers = ");
+                 scanf("%d",&N1[i]);
+            }
+            return 0;
  }
   int Find_Even_Cnt(int N2[])
   {
-       int i = 0,E_Cnt = 0;
+            int i = 0,E_Cnt = 0;
 
-       for(i = 0;i < Cnt;i++)
-       {
-            if(N2[i] % 2 == 0)
+            for(i = 0;i < Cnt;i++)
             {
-                 E_Cnt++;
+                 if(N2[i] % 2 == 0)
+                 {
+                      E_Cnt++;
+                 }
             }
-       }
-       return E_Cnt;
+            return E_Cnt;
 
   }
 
