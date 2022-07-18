537e4e78fd9b30498ba9c22c43ec3921f542d194
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-18 22:52:30 +0530

    Update 01.Create Array Without Initilization & Display All Elements With Function.c

--- Array With Function/01.Create Array Without Initilization & Display All Elements With Function.c
+++ Array With Function/01.Create Array Without Initilization & Display All Elements With Function.c
@@ -5,20 +5,20 @@
 
  int main()
  {
-        Array_Element();
+          Array_Element();
 
  }
 int Array_Element()
 {
-        int Arr[5]={0};
+          int Arr[5]={0};
 
-        printf("\n Frist  Element = %d",Arr[0]);
-        printf("\n Second Element = %d",Arr[1]);
-        printf("\n Third  Element = %d",Arr[2]);
-        printf("\n Fourth Element = %d",Arr[3]);
-        printf("\n Fifth  Element = %d",Arr[4]);
+          printf("\n Frist  Element = %d",Arr[0]);
+          printf("\n Second Element = %d",Arr[1]);
+          printf("\n Third  Element = %d",Arr[2]);
+          printf("\n Fourth Element = %d",Arr[3]);
+          printf("\n Fifth  Element = %d",Arr[4]);
 
-        return 0;
+          return 0;
 
 }
 
