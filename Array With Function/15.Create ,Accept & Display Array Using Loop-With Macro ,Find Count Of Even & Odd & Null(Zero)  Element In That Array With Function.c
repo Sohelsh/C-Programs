df16b09a8d22c028a2974ecba5e4f7157fa0a6dc
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-18 23:04:19 +0530

    Update 15.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even & Odd & Null(Zero)  Element In That Array With Function.c

--- Array With Function/15.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even & Odd & Null(Zero)  Element In That Array With Function.c
+++ Array With Function/15.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even & Odd & Null(Zero)  Element In That Array With Function.c
@@ -9,66 +9,66 @@
 
   int main()
   {
-          int Arr[Size] = {0};
+            int Arr[Size] = {0};
 
-          Accepts_Element(Arr);
+            Accepts_Element(Arr);
 
-          getch();
+            getch();
 
-          printf("\n Count Of Even Elements Is = %d",Find_Even_Cnt(Arr));
-          printf("\n Count Of Odd Elements Is = %d",Find_Odd_Cnt(Arr));
-          printf("\n Count Of Null(0) Elements Is = %d",Find_Zero_Cnt(Arr));
+            printf("\n Count Of Even Elements Is = %d",Find_Even_Cnt(Arr));
+            printf("\n Count Of Odd Elements Is = %d",Find_Odd_Cnt(Arr));
+            printf("\n Count Of Null(0) Elements Is = %d",Find_Zero_Cnt(Arr));
   }
   int Accepts_Element(int N1[])
   {
-          int i = 0;
+            int i = 0;
 
-          for(i = 0;i < Size;i++)
-          {
-             printf("\n Entar Elements = ");
-             scanf("%d",&N1[i]);
-          }
-          return;
+            for(i = 0;i < Size;i++)
+            {
+               printf("\n Entar Elements = ");
+               scanf("%d",&N1[i]);
+            }
+            return;
 
   }
   int Find_Even_Cnt(int N2[])
   {
-          int i = 0, E_Cnt = 0;
+            int i = 0, E_Cnt = 0;
 
-          for(i = 0;i < Size;i++)
-          {
-             if(N2[i] % 2 == 0 && N2[i] != 0)
-             {
-                  E_Cnt++;
-             }
-          }
-          return E_Cnt;
+            for(i = 0;i < Size;i++)
+            {
+               if(N2[i] % 2 == 0 && N2[i] != 0)
+               {
+                    E_Cnt++;
+               }
+            }
+            return E_Cnt;
 
   }
   int Find_Odd_Cnt(int N3[])
   {
-          int i = 0, O_Cnt = 0;
+            int i = 0, O_Cnt = 0;
 
-           for(i = 0;i < Size;i++)
-          {
-             if(N3[i] % 2 == 1)
-             {
-                  O_Cnt++;
-             }
-          }
-          return O_Cnt;
+             for(i = 0;i < Size;i++)
+            {
+               if(N3[i] % 2 == 1)
+               {
+                    O_Cnt++;
+               }
+            }
+            return O_Cnt;
 
   }
   int Find_Zero_Cnt(int N4[])
   {
-           int i = 0, Z_Cnt = 0;
+             int i = 0, Z_Cnt = 0;
 
-           for(i = 0;i < Size;i++)
-          {
-             if(N4[i] == 0)
-             {
-                  Z_Cnt++;
-             }
-          }
-          return Z_Cnt;
+             for(i = 0;i < Size;i++)
+            {
+               if(N4[i] == 0)
+               {
+                    Z_Cnt++;
+               }
+            }
+            return Z_Cnt;
   }
