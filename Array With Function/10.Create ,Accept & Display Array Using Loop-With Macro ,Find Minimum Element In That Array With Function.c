71e6d551fa3b6e2791e28b4f13b5b7e1fc9c6a99
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-18 22:56:21 +0530

    Update 10.Create ,Accept & Display Array Using Loop-With Macro ,Find Minimum Element In That Array With Function.c

--- Array With Function/10.Create ,Accept & Display Array Using Loop-With Macro ,Find Minimum Element In That Array With Function.c
+++ Array With Function/10.Create ,Accept & Display Array Using Loop-With Macro ,Find Minimum Element In That Array With Function.c
@@ -7,36 +7,36 @@
 
  int main()
  {
-      int Arr[Cnt]={};
+          int Arr[Cnt]={};
 
-      Accepts_Element(Arr);
+          Accepts_Element(Arr);
 
-      getch;
+          getch;
 
-      printf("\n Minimum Number Is = %d ",Find_Minimum_Num(Arr));
+          printf("\n Minimum Number Is = %d ",Find_Minimum_Num(Arr));
  }
  int Accepts_Element(int N1[])
  {
-      int i = 0;
+          int i = 0;
 
-      for(i = 0;i < Cnt; i++)
-      {
-           printf("\n Entar Elements = ");
-           scanf("%d",&N1[i]);
-      }
-      return;
+          for(i = 0;i < Cnt; i++)
+          {
+               printf("\n Entar Elements = ");
+               scanf("%d",&N1[i]);
+          }
+          return;
  }
  int Find_Minimum_Num(int N2[])
  {
-      int i = 0,Min = 0;
+          int i = 0,Min = 0;
 
-      for(i = 0;i < Cnt; i++)
-      {
-         if(Min > N2[i])
-         {
-              Min = N2[i];
-         }
-      }
-       return;
+          for(i = 0;i < Cnt; i++)
+          {
+             if(Min > N2[i])
+             {
+                  Min = N2[i];
+             }
+          }
+           return;
  }
 
