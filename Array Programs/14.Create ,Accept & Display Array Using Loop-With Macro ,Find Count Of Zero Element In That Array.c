5988011c2fc0ba4ccadf9a206230e1ed8c50b65c
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-19 22:56:03 +0530

    Add files via upload

---
+++ Array Programs/14.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Zero Element In That Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 6
+
+ int main()
+ {
+           int Arr[Size] = {0},i = 0,Z_Cnt = 0;
+
+           for(i = 0;i < Size;i++)
+           {
+                printf("\n %d.Entar Element = ",i+101);
+                scanf("%d",&Arr[i]);
+           }
+           for(i = 0;i < Size;i++)
+           {
+                printf("\n\n %d.Element Is = %d",i+101,Arr[i]);
+           }
+           for(i = 0;i < Size;i++)
+           {
+                if(Arr[i] == 0)
+                {
+                     Z_Cnt++;
+                }
+           }
+
+           printf("\n\n Count Of Zero Is = %d",Z_Cnt);
+
+           getch();
+           return 0;
+ }
