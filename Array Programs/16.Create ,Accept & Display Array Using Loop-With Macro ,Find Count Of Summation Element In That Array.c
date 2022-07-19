1da816a65e32c29b1b3458d06de63bffecaec8f0
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-19 22:57:14 +0530

    Add files via upload

---
+++ Array Programs/16.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Summation Element In That Array.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 6
+
+ int main()
+ {
+      int Arr[Size] = {0},i = 0,Ele_Sum = 0;
+
+      for(i = 0;i < Size;i++)
+      {
+           printf("\n %d.Entar Elements = ",i+1);
+           scanf("%d",&Arr[i]);
+      }
+      for(i = 0;i < Size;i++)
+      {
+           printf("\n %d.Elemets Is = %d",i+1,Arr[i]);
+      }
+      for(i = 0;i < Size;i++)
+      {
+          Ele_Sum = Ele_Sum + Arr[i];
+      }
+
+      printf("\n\n Summation Of Element Is = %d",Ele_Sum);
+
+      getch();
+      return 0;
+ }
