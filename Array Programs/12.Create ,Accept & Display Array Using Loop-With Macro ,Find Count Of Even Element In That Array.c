7a6ad29dd457d413c6ed153531f42b22bb26947d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-19 22:53:28 +0530

    Add files via upload

---
+++ Array Programs/12.Create ,Accept & Display Array Using Loop-With Macro ,Find Count Of Even Element In That Array.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+#define Size 5
+
+ int main()
+ {
+           int Arr[Size] = {0},i = 0,E_Cnt = 0;
+
+           for(i = 0;i < Size;i++)
+           {
+                printf("\n %d.Entar Elements = ",i+1);
+                scanf("%d",&Arr[i]);
+           }
+           for(i = 0;i < Size;i++)
+           {
+                printf("\n Element Is = %d",Arr[i]);
+           }
+           for(i = 0;i < Size;i++)
+           {
+                if(Arr[i] % 2 == 0)
+                {
+                     E_Cnt++;
+                }
+           }
+
+           printf("\n\n Count Of Even Elements Is = %d",E_Cnt);
+
+           getch();
+           return 0;
+ }
