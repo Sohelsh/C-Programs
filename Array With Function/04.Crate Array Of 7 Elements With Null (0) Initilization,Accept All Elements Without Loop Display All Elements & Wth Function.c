ec82340c130451e0768d0c5846db5e5e05d12e76
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-20 22:59:39 +0530

    Add files via upload

---
+++ Array With Function/04.Crate Array Of 7 Elements With Null (0) Initilization,Accept All Elements Without Loop Display All Elements & Wth Function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void Display_Elements(int Arr[]);
+
+ int main()
+ {
+      int Value[7] = {0};
+
+      Display_Elements(Value);
+
+      return 0;
+ }
+ void Display_Elements(int Arr[])
+ {
+        printf("\n Frist   Element = %d",Arr[0]);
+        printf("\n Second  Element = %d",Arr[1]);
+        printf("\n Third   Element = %d",Arr[2]);
+        printf("\n Fourth  Element = %d",Arr[3]);
+        printf("\n Fifth   Element = %d",Arr[4]);
+        printf("\n Sixth   Element = %d",Arr[5]);
+        printf("\n Sevnth  Element = %d",Arr[6]);
+
+        return;
+ }
