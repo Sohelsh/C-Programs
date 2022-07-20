07fab5210cdab276f62b53cd51a85504d4c3db09
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-20 23:04:11 +0530

    Add files via upload

---
+++ Function/03.Power Of Number Using Function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int Power_Of_Number(int,int);
+
+ int main()
+ {
+      int No1 = 0,No2 = 0,Res = 0;
+
+      printf("\n Entar No1 Raise To No2 = ");
+      scanf("%d%d",&No1,&No2);
+
+     Res = Power_Of_Number(No1,No2);
+
+     printf("\n\n Power Of Number (%d)^(%d)= %d",No1,No2,Res);
+ }
+ int Power_Of_Number(N1,N2)
+ {
+      int Pow =0, i = 0;
+
+      for(i = 1,Pow = 1; i <= N2;i++)
+      {
+           Pow = Pow * N1;
+      }
+
+      return Pow;
+ }
+
