5255156bfd362d4c1ab9a05bebe84b6263a82351
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-20 23:05:27 +0530

    Add files via upload

---
+++ Function/04.Count Of Given Number Using Function.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int Count_Of_Number(int);
+
+ int main()
+ {
+      int No =0,Res = 0;
+
+      printf("\n Entar A Number = ");
+      scanf("%d",&No);
+
+      Res = Count_Of_Number(No);
+
+      printf("\n Count Of Number %d = %d",No,Res);
+ }
+ int Count_Of_Number(Num)
+ {
+      int Dig = 0,Cnt = 0;
+
+      while(Num != 0)
+      {
+           Dig = Num % 10;
+           Num = Num / 10;
+           Cnt++;
+      }
+      return Cnt;
+ }
