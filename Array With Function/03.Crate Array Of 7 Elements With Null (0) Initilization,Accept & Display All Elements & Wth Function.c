5465a03da6c2fbffc1b67120a431974d6f4eff88
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-20 22:59:08 +0530

    Add files via upload

---
+++ Array With Function/03.Crate Array Of 7 Elements With Null (0) Initilization,Accept & Display All Elements & Wth Function.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+
+ void Display_Elements(int N1[] );
+
+ int main()
+ {
+      int Arr[7] = {0};
+
+      Display(Arr);
+
+      return 0;
+ }
+  void Display(int N1[])
+ {
+      int i = 0;
+
+      for(i = 0;i < 7;i++)
+      {
+           printf("\n %d.Element Is = %d ",i+1,N1[i]);
+      }
+
+      getch();
+      return;
+ }
