d60f6688489107334f619c8ffe38e5a67d42b187
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-23 23:50:59 +0530

    Update 07.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Location If Found.c

--- Array Programs/07.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Location If Found.c
+++ Array Programs/07.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Location If Found.c
@@ -4,44 +4,44 @@
 
  int main()
  {
-     int Arr[Cnt] = {},i = 0,src = 0;
+               int Arr[Cnt] = {},i = 0,src = 0;
 
-     for(i = 0;i < Cnt;i++)
-     {
-         printf("\n %d.Entar A Element = ",i+101);
-         scanf("%d",&Arr[i]);
-     }
+               for(i = 0;i < Cnt;i++)
+               {
+                 printf("\n %d.Entar A Element = ",i+101);
+                 scanf("%d",&Arr[i]);
+               }
 
-      system("cls");
-      getch();
+               system("cls");
+               getch();
 
-     for(i = 0;i < Cnt;i++)
-     {
-         printf("\n Element Is %d = %d",i+101,Arr[i]);
-     }
+               for(i = 0;i < Cnt;i++)
+               {
+                 printf("\n Element Is %d = %d",i+101,Arr[i]);
+               }
 
-     getch();
-     system("cls");
+               getch();
+               system("cls");
 
-     printf("\n Entar Search Element = ");
-     scanf("%d",&src);
+               printf("\n Entar Search Element = ");
+               scanf("%d",&src);
 
-     for(i = 0;i < Cnt;i++)
-     {
-         if(src == Arr[i])
-         {
-             break;
-         }
-     }
-      if(i < Cnt)
-      {
-          printf("\n Element Is Found :'%d'& Location Is = %d",src,i+101);
-      }
-      else
-      {
-          printf("\n Element Is Not Fount ");
-      }
+               for(i = 0;i < Cnt;i++)
+               {
+                 if(src == Arr[i])
+                 {
+                     break;
+                 }
+               }
+               if(i < Cnt)
+               {
+                  printf("\n Element Is Found :'%d'& Location Is = %d",src,i+101);
+               }
+               else
+               {
+                  printf("\n Element Is Not Fount ");
+               }
 
-       getch();
-       return 0;
+               getch();
+               return 0;
  }
