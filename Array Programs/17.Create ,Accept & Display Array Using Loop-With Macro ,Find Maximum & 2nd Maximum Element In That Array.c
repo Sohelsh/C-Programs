aae264a8c4524a4b773045e2212262597b9b20cf
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-23 23:43:57 +0530

    Update 17.Create ,Accept & Display Array Using Loop-With Macro ,Find Maximum & 2nd Maximum Element In That Array.c

--- Array Programs/17.Create ,Accept & Display Array Using Loop-With Macro ,Find Maximum & 2nd Maximum Element In That Array.c
+++ Array Programs/17.Create ,Accept & Display Array Using Loop-With Macro ,Find Maximum & 2nd Maximum Element In That Array.c
@@ -4,32 +4,32 @@
 
  int main()
  {
-      int Arr[Size] = {0},i = 0,Max = 0,S_Max = 0;
+            int Arr[Size] = {0},i = 0,Max = 0,S_Max = 0;
 
-      for(i = 0;i < Size;i++)
-      {
-           printf("\n Entar Elements = ");
-           scanf("%d",&Arr[i]);
-      }
-      for(i = 0;i < Size;i++)
-      {
-           printf("\n Element Is = %d ",Arr[i]);
-      }
-      for(i = 0;i < Size;i++)
-      {
-           if(Max < Arr[i])
-           {
-                Max = Arr[i];
-           }
-           else if (Max != Arr[i] && S_Max < Arr[i])
-           {
-                S_Max = Arr[i];
-           }
-      }
+            for(i = 0;i < Size;i++)
+            {
+                 printf("\n Entar Elements = ");
+                 scanf("%d",&Arr[i]);
+            }
+            for(i = 0;i < Size;i++)
+            {
+                 printf("\n Element Is = %d ",Arr[i]);
+            }
+            for(i = 0;i < Size;i++)
+            {
+                 if(Max < Arr[i])
+                 {
+                      Max = Arr[i];
+                 }
+                 else if (Max != Arr[i] && S_Max < Arr[i])
+                 {
+                      S_Max = Arr[i];
+                 }
+            }
 
-      printf("\n\n Maximum Number Is = %d",Max);
-      printf("\n\n Second Maximum Number Is = %d",S_Max);
+            printf("\n\n Maximum Number Is = %d",Max);
+            printf("\n\n Second Maximum Number Is = %d",S_Max);
 
-      getch();
-      return 0;
+            getch();
+            return 0;
  }
