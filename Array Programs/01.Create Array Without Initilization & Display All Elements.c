be04b79954b8cecab5dd19a4b50f2d9d56472b16
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-23 23:46:10 +0530

    Update 01.Create Array Without Initilization & Display All Elements.c

--- Array Programs/01.Create Array Without Initilization & Display All Elements.c
+++ Array Programs/01.Create Array Without Initilization & Display All Elements.c
@@ -3,13 +3,13 @@
 
  int main()
  {
-     int Value[5] ={0};
+           int Value[5] ={0};
 
-        printf("\n Frist  Element = %d",Value[0]);
-        printf("\n Second Element = %d",Value[1]);
-        printf("\n Third  Element = %d",Value[2]);
-        printf("\n Fourth Element = %d",Value[3]);
-        printf("\n Fifth  Element = %d",Value[4]);
+           printf("\n Frist  Element = %d",Value[0]);
+           printf("\n Second Element = %d",Value[1]);
+           printf("\n Third  Element = %d",Value[2]);
+           printf("\n Fourth Element = %d",Value[3]);
+           printf("\n Fifth  Element = %d",Value[4]);
 
-        getch();
+           getch();
  }
