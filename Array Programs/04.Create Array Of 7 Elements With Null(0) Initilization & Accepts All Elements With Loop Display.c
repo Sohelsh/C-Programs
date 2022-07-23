cf03bc579b3c01f4c004ba1b7deb071c61212642
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-23 23:47:33 +0530

    Update 04.Create Array Of 7 Elements With Null(0) Initilization & Accepts All Elements With Loop Display.c

--- Array Programs/04.Create Array Of 7 Elements With Null(0) Initilization & Accepts All Elements With Loop Display.c
+++ Array Programs/04.Create Array Of 7 Elements With Null(0) Initilization & Accepts All Elements With Loop Display.c
@@ -4,16 +4,16 @@
 
  int main()
  {
-     int Value[7]={11,22,33,44,55,66,77};
+           int Value[7]={11,22,33,44,55,66,77};
 
-        printf("\n Frist   Element = %d",Value[0]);
-        printf("\n Second  Element = %d",Value[1]);
-        printf("\n Third   Element = %d",Value[2]);
-        printf("\n Fourth  Element = %d",Value[3]);
-        printf("\n Fifth   Element = %d",Value[4]);
-        printf("\n Sixth   Element = %d",Value[5]);
-        printf("\n Sevnth  Element = %d",Value[6]);
+           printf("\n Frist   Element = %d",Value[0]);
+           printf("\n Second  Element = %d",Value[1]);
+           printf("\n Third   Element = %d",Value[2]);
+           printf("\n Fourth  Element = %d",Value[3]);
+           printf("\n Fifth   Element = %d",Value[4]);
+           printf("\n Sixth   Element = %d",Value[5]);
+           printf("\n Sevnth  Element = %d",Value[6]);
 
-        getch();
+           getch();
  }
 
