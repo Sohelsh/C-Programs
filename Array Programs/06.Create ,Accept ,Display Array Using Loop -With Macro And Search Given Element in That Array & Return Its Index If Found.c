a6a4175482c5df2279fd9a0d771295bb8a3bbf6c
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-23 23:49:35 +0530

    Update 06.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Index If Found.c

--- Array Programs/06.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Index If Found.c
+++ Array Programs/06.Create ,Accept ,Display Array Using Loop -With Macro And Search Given Element in That Array & Return Its Index If Found.c
@@ -5,53 +5,53 @@
 
  int main()
  {
-     int Arr[Cnt]={},i = 0, Ser = 0;
+        int Arr[Cnt]={},i = 0, Ser = 0;
 
-     printf("\n\t******Entar Element******");
-     for(i = 0;i < Cnt;i++)
-     {
-         Up:
-         printf("\n %d.Entar A Element = ",i+1);
-         scanf("%d",&Arr[i]);
+        printf("\n\t******Entar Element******");
+        for(i = 0;i < Cnt;i++)
+        {
+            Up:
+            printf("\n %d.Entar A Element = ",i+1);
+            scanf("%d",&Arr[i]);
 
-         if(Arr[i] <= 0)
+            if(Arr[i] <= 0)
+            {
+                goto Up;
+            }
+        }
+        system("cls");
+        getch();
+
+
+        printf("\n\t******Element List******");
+        for(i = 0;i < Cnt;i++)
+        {
+            printf("\n Element Of %d = %d",i+1,Arr[i]);
+        }
+        getch();
+        system("cls");
+
+        printf("\n******Search Element******");
+
+        printf("\n Entar Search Element = ");
+        scanf("%d",&Ser);
+
+        for(i = 0;i < Cnt;i++)
+        {
+            if(Arr[i] == Ser)
+            {
+                break;
+            }
+        }
+         if(i < Cnt)
          {
-             goto Up;
+             printf("\n Element Is Found ");
          }
-     }
-     system("cls");
-     getch();
-
-
-     printf("\n\t******Element List******");
-     for(i = 0;i < Cnt;i++)
-     {
-         printf("\n Element Of %d = %d",i+1,Arr[i]);
-     }
-     getch();
-     system("cls");
-
-     printf("\n******Search Element******");
-
-     printf("\n Entar Search Element = ");
-     scanf("%d",&Ser);
-
-     for(i = 0;i < Cnt;i++)
-     {
-         if(Arr[i] == Ser)
+         else
          {
-             break;
+             printf("\n Element Is Not Found ");
          }
-     }
-      if(i < Cnt)
-      {
-          printf("\n Element Is Found ");
-      }
-      else
-      {
-          printf("\n Element Is Not Found ");
-      }
 
-      return 0;
-      getch();
+         return 0;
+         getch();
  }
