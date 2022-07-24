13aafef1032111726ce075f62485a70bd3af8615
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-07-24 23:38:27 +0530

    Update 03.Print Table Of Inputted Numbar In Reverse .c

--- New Digit Program/03.Print Table Of Inputted Numbar In Reverse .c
+++ New Digit Program/03.Print Table Of Inputted Numbar In Reverse .c
@@ -3,19 +3,19 @@
 
 int main()
 {
-     int Cnt = 0, No = 0;
+      int Cnt = 0, No = 0;
 
-     printf("\n Entar A Numbar = ");
-     scanf("%d",&No);
+      printf("\n Entar A Numbar = ");
+      scanf("%d",&No);
 
-     for(Cnt = 10;Cnt >= 1;Cnt--)
-     {
+      for(Cnt = 10;Cnt >= 1;Cnt--)
+       {
           printf("%d",No*Cnt);
-
           printf("\n");
-     }
+       }
 
-      return 0;
-      getch();
+
+       return 0;
+       getch();
 
 }
