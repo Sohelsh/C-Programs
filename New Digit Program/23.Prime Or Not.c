0a509b219a978cb457dff1b2ab3ad60e7a0e662e
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-08-02 23:48:31 +0530

    Add files via upload

---
+++ New Digit Program/23.Prime Or Not.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+ int main()
+ {
+     int iNo = 0,i = 0;
+
+     printf("\n Entar Number = ");
+     scanf("%d",&iNo);
+
+     for(i = 2;i < iNo;i++)
+     {
+         if(iNo % i== 0)
+         {
+             break;
+         }
+     }
+     if(i == iNo)
+     {
+         printf("\n Number Is Prime = %d",iNo);
+     }
+     else
+     {
+         printf("\n Number Is Not Prime = %d",iNo);
+     }
+     getch();
+     return 0;
+
+ }
