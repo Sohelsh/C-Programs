be063c0b484028d64684a0c70edc60e3054932d0
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-08-04 23:55:10 +0530

    Update 01.Function-Accepts Nothing &Return Nothing.c

--- Function/01.Function-Accepts Nothing &Return Nothing.c
+++ Function/01.Function-Accepts Nothing &Return Nothing.c
@@ -1,28 +1,29 @@
 #include<stdio.h>
 #include<conio.h>
+
  void Add(void);
 
- int main()
- {
-     printf("\n\t***** Welcome ***** ");
+int main()
+{
+      printf("\n\t***** Welcome ***** ");
 
-     Add();
+      Add();
 
-     printf("\n\t  Thanks ");
+      printf("\n\t  Thanks ");
 
-     getch();
-     return 0;
- }
- void Add()
- {
-     int No1 = 0,No2 = 0,Res = 0;
+      getch();
+      return 0;
+}
+void Add()
+{
+      int No1 = 0,No2 = 0,Res = 0;
 
-     printf("\n\n Entar 2 Numbars = ");
-     scanf("%d%d",&No1,&No2);
+      printf("\n\n Entar 2 Numbars = ");
+      scanf("%d%d",&No1,&No2);
 
-     Res = No1 + No2;
+      Res = No1 + No2;
 
-     printf("\n Addition Of Givan Numbar %d + %d = %d",No1,No2,Res);
+      printf("\n Addition Of Givan Numbar %d + %d = %d",No1,No2,Res);
 
-     return;
- }
+      return;
+}
