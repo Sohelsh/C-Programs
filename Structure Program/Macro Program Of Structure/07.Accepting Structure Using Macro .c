8147387e0ffea4dcc1595c536a26ad1e1ae07e81
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-08-04 23:51:26 +0530

    Update 07.Accepting Structure Using Macro .c

--- Structure Program/Macro Program Of Structure/07.Accepting Structure Using Macro .c
+++ Structure Program/Macro Program Of Structure/07.Accepting Structure Using Macro .c
@@ -4,52 +4,52 @@
 #define Hos_P 3          ///Macro
 
 
- struct Hospital
- {
-     char Patient_Name[20];
-     int Patient_Age;
-     char Patient_From[20];
+struct Hospital
+{
+       char Patient_Name[20];
+       int Patient_Age;
+       char Patient_From[20];
 
- };
+};
 int main()
 {
-          int i =0;
+        int i =0;
 
-          struct Hospital Hos[4];
+        struct Hospital Hos[4];
 
-          for(i = 0;i < Hos_P;i++)
-          {
-              printf("\n\t %d Patient Detilas ",i+1);
+        for(i = 0;i < Hos_P;i++)
+        {
+        printf("\n\t %d Patient Detilas ",i+1);
 
-              printf("\n\n Entar Patient Name = ");
-              gets(Hos[i].Patient_Name);
+        printf("\n\n Entar Patient Name = ");
+        gets(Hos[i].Patient_Name);
 
-              fflush(stdin);
+        fflush(stdin);
 
-              printf("\n Entar Patient Age = ");
-              scanf("%d",&Hos[i].Patient_Age);
+        printf("\n Entar Patient Age = ");
+        scanf("%d",&Hos[i].Patient_Age);
 
-              fflush(stdin);
+        fflush(stdin);
 
-              printf("\n Entar Patient From = ");
-              gets(Hos[i].Patient_From);
+        printf("\n Entar Patient From = ");
+        gets(Hos[i].Patient_From);
 
-              fflush(stdin);
+        fflush(stdin);
 
-              printf("\n ---------------------------------------------- ");
+        printf("\n ---------------------------------------------- ");
 
-              system("cls");
-          }
+        system("cls");
+        }
 
-          for(i = 0;i < Hos_P;i++)
-          {
-              printf("\n******* %d Patients Detalis******* ",i+1);
+        for(i = 0;i < Hos_P;i++)
+        {
+        printf("\n******* %d Patients Detalis******* ",i+1);
 
 
-              printf("\n\n Patient Name= %s \n\n Patient Age= %d \n\n Patient From = %s",Hos[i].Patient_Name,Hos[i].Patient_Age,Hos[i].Patient_From);
+        printf("\n\n Patient Name= %s \n\n Patient Age= %d \n\n Patient From = %s",Hos[i].Patient_Name,Hos[i].Patient_Age,Hos[i].Patient_From);
 
-          }
+        }
 
-          getch();
-          return 0;
+        getch();
+        return 0;
 }
