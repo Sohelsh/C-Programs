25bcdc608de013d59f8dd3c9243a9fdc0c67e54d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2022-08-29 22:38:01 +0530

    Add files via upload

---
+++ New Digit Program/27.Factorial Program Without Loop And Recursion.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Factorial_Of_Number(int Num);
+int main()
+{
+    int No = 0;
+
+    printf("\n Enter Number = ");
+    scanf("%d",&No);
+
+    printf("Fatorial Of Number = %ld",Factorial_Of_Number(No));
+
+    return 0;
+}
+int Factorial_Of_Number(int Num)
+{
+    long int Fact = 1;
+
+    up:
+    if(Num > 1)
+    {
+       Fact = Fact * Num;
+       Num--;
+       goto up;
+    }
+    return Fact;
+}
