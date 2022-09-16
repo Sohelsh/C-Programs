3e99e2f4c8ab1fa5cb94fa92c494a127a39b45fe
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-16 23:49:19 +0530

    Added File

--- New Digit Program/14.Percentage Of Given Numbers.c
+++ New Digit Program/14.Percentage Of Given Numbers.c
@@ -3,20 +3,22 @@
 
  int main()
  {
-      int No1 = 0,No2 =0,Per = 0;
+          int No1 = 0,No2 =0,Per = 0;
 
-      printf("\n Entar A Number Of Percentage = ");
-      scanf("%d",&No1);
+          printf("\n Entar A Number Of Percentage = ");
+          scanf("%d",&No1);
 
-      printf("\n How Much Percent = ");
-      scanf("%d",&No2);
+          printf("\n How Much Percent = ");
+          scanf("%d",&No2);
 
-      Per = (No1 * No2) /100;
+          Per = (No1 * No2) /100;
 
-      printf("\n Addition  Of |%d| & |%d| = %d",No1,No2,Per);
 
-      getch();
-      return 0;
+          printf("\n Addition  Of |%d| & |%d| = %d",No1,No2,Per);
+
+
+          getch();
+          return 0;
 
  }
 
