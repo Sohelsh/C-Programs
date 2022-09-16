f98dd9bcd0a9ba99b1e5301101e31fe3c6e95762
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-16 23:48:19 +0530

    Added File

--- New Digit Program/05.Write Program To Display Table Of Numbar In Reverse10-5.c
+++ New Digit Program/05.Write Program To Display Table Of Numbar In Reverse10-5.c
@@ -3,18 +3,19 @@
 
  int main()
  {
-    int R = 0,C = 0;
+        int R = 0,C = 0;
 
-    for(R = 10;R >= 1;R--)
-    {
-         for(C = 10; C >= 5;C--)
-         {
+        for(R = 10;R >= 1;R--)
+        {
+          for(C = 10; C >= 5;C--)
+           {
               printf("%3d",R*C);
-         }
-         printf("\n");
-    }
+           }
 
-    getch();
-    return 0;
+             printf("\n");
+         }
+
+        getch();
+        return 0;
 
  }
