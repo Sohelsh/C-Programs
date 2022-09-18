685b274ec6a66803e2bd1b87cf4d2664dccdc964
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-18 23:43:41 +0530

    Added File

--- New Digit Program/26.Write A Program Declare Number Is Perfect Or Not.c
+++ New Digit Program/26.Write A Program Declare Number Is Perfect Or Not.c
@@ -3,28 +3,28 @@
 
  int main()
  {
-     int iValue = 0,i = 1, Sum = 0;
+         int iValue = 0,i = 1, Sum = 0;
 
-     printf("\n Entar Number = ");
-     scanf("%d",&iValue);
+         printf("\n Entar Number = ");
+         scanf("%d",&iValue);
 
-     while(iValue > i)
-     {
-         if(iValue % i == 0 )
+         while(iValue > i)
          {
-             Sum += i;
+             if(iValue % i == 0 )
+             {
+                 Sum += i;
+             }
+             i++;
          }
-         i++;
-     }
-     if(Sum == iValue)
-     {
-         printf("\n Number Is Perfect ");
-     }
-     else
-     {
-         printf("\n Number Is Not Perfect ");
-     }
+         if(Sum == iValue)
+         {
+             printf("\n Number Is Perfect ");
+         }
+         else
+         {
+             printf("\n Number Is Not Perfect ");
+         }
 
-     return 0;
-     getch();
+         return 0;
+         getch();
  }
