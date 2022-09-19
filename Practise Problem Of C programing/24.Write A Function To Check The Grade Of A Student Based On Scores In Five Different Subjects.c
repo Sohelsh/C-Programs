0317c00b9fa66d64d328b77aad588c9da3353c4c
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-19 23:26:36 +0530

    Added File

---
+++ Practise Problem Of C programing/24.Write A Function To Check The Grade Of A Student Based On Scores In Five Different Subjects.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<conio.h>
+
+int Check_Scores(int Num[])
+{
+    int i = 0,Sum = 0;
+
+    for(i = 0;i < 5;i++)
+    {
+        Sum = Sum + Num[i];
+    }
+
+    Sum = Sum / 5;
+    return Sum;
+}
+int main()
+{
+    int Mark[5] = {0}, i = 0,Res = 0;
+
+    for(i = 0;i < 5;i++)
+    {
+        printf("\n Enter Mark Of %d Subject = ",i+ 1);
+        scanf("%d",&Mark[i]);
+    }
+     Res = Check_Scores(Mark);
+
+     if(Res >= 80)
+     {
+         printf("\n Grade Of Student = A");
+     }
+     else if(Res >=60 && Res < 80)
+     {
+         printf("\n Grade Of Student = B");
+     }
+     else if(Res >= 40 && Res < 60)
+     {
+         printf("\n Grade Of Student = C");
+     }
+     else
+     {
+         printf("\n Grade Of Student = D");
+     }
+
+    return 0;
+    getch();
+}
