e601081d9b78985c6957c76e7353d068c83e720e
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-19 23:34:15 +0530

    Modified Program

--- String/03. White Space Count In String.c
+++ String/03. White Space Count In String.c
@@ -3,24 +3,23 @@
 
 int main()
 {
-  char cSrc[30]={'\0'};
-  int i =0,scout =0;
-  
-  printf("Enter A String = ");
-  gets(cSrc);
-  
-     while(cSrc[i] != '\0')
-     {
-        if(cSrc[i] == ' ')
-       {
-          scout++;
-       }
-        i++;
-     }
- 
-   
-     printf("\n Space Count is %d",scout);
-   
-  getch();
-  return 0;
+        char cSrc[30]={'\0'};
+        int i = 0,scout = 0;
+
+        printf("Enter A String = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+            if(cSrc[i] == ' ')
+            {
+            scout++;
+            }
+            i++;
+        }
+
+        printf("\n Space Count is = %d",scout);
+
+        getch();
+        return 0;
 }
