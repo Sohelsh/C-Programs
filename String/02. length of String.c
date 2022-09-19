52b218947f9edfe0a725bab94ae64c52ef2db806
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-19 23:32:59 +0530

    Modifird Program

--- String/02. length of String.c
+++ String/02. length of String.c
@@ -5,21 +5,22 @@
 
 int main()
 {
-   char Carr[40]={};
-   int i = 0;
-   
-   puts("Enter A String = ");
-   gets(Carr);
-   
-   for(i =0;i<=40;i++)
-    {
-      if(Carr[i] == '\0')
-      {
-        break;
-      }
-   
-    }
-    printf("\n Length of String is %d",i);
-   getch();
-  return 0;
+       char Carr[40]={};
+       int i = 0;
+
+       puts("Enter A String = ");
+       gets(Carr);
+
+       for(i =0;i<=40;i++)
+        {
+          if(Carr[i] == '\0')
+          {
+            break;
+          }
+
+        }
+        printf("\n Length of String is %d",i);
+
+      getch();
+      return 0;
 }
