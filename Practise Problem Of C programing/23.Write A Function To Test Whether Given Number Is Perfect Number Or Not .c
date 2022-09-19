4092c9afc809fe38d1fa096803b0c8c9c67b5be4
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-19 23:21:01 +0530

    Added File

---
+++ Practise Problem Of C programing/23.Write A Function To Test Whether Given Number Is Perfect Number Or Not .c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+#include<conio.h>
+
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL Calulate_Perfect_Num(int Num)
+{
+        int i = 1,Sum = 0;
+
+        while(i < Num)
+        {
+            if(Num % i == 0)
+            {
+                Sum = Sum + i;
+            }
+            i++;
+        }
+         if(Sum == Num)
+        {
+            return TRUE;
+        }
+        else
+        {
+            return FALSE;
+        }
+}
+int main()
+{
+        int No = 0;
+
+        printf("\n Enter Number = ");
+        scanf("%d",&No);
+
+        printf("\n Output Is %d ", Calulate_Perfect_Num(No));
+
+        return 0;
+        getch();
+
+}
