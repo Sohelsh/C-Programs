6e66ceb88074b415c1a5458ac0469da42e523ca1
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-20 23:52:40 +0530

    Modified Program

--- Fork Assignment/03. Assignment.c
+++ Fork Assignment/03. Assignment.c
@@ -2,27 +2,27 @@
 #include <conio.h>
 
 //Compiler version gcc  6.3.0
-// Count Of Zero 
+// Count Of Zero
 int main()
 {
-  int Num[15] ={'\0'};
-  int i = 0 ,Z_Cnt = 0;
-  
-  printf("Enter A Number = ");
-  gets(Num);
-  
-  while(Num[i] != '\0')
-  {
-     if(Num[i] == '0')
-     {
-       Z_Cnt++;
-     }  
-     
-     i++;     
-  }
-   
-  printf("Given Number Zero Count Is = %d",Z_Cnt);
-  
-  getch();
-  return 0;
+      int Num[15] ={'\0'};
+      int i = 0 ,Z_Cnt = 0;
+
+      printf("Enter A Number = ");
+      gets(Num);
+
+      while(Num[i] != '\0')
+      {
+             if(Num[i] == '0')
+             {
+                Z_Cnt++;
+             }
+
+             i++;
+      }
+
+      printf("Given Number Zero Count Is = %d",Z_Cnt);
+
+      getch();
+      return 0;
 }
