32482630372d6f530fe3d2f2856c87ae3c01f9d9
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-22 22:53:53 +0530

    Added File

--- New Basic Program/05D. Use Of Size Of Operater.c
+++ New Basic Program/05D. Use Of Size Of Operater.c
@@ -3,15 +3,15 @@
 
 int main()
 {
-    printf("\n Size Of Int  =   %d",sizeof(int));
+        printf("\n Size Of Int  =   %d",sizeof(int));
 
-    printf("\n Size Of Float =  %d",sizeof(float));
+        printf("\n Size Of Float =  %d",sizeof(float));
 
-    printf("\n Size Of Double = %d",sizeof(double));
+        printf("\n Size Of Double = %d",sizeof(double));
 
-    printf("\n Size Of Char =   %d",sizeof(char));
+        printf("\n Size Of Char =   %d",sizeof(char));
 
-    getch();
-    return 0;
-    
+        getch();
+        return 0;
+
 }
