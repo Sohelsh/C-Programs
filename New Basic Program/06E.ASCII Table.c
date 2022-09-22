79204f7f48b364610161ccd303e5e875fdb04973
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-22 23:07:55 +0530

    Added File

--- New Basic Program/06E.ASCII Table.c
+++ New Basic Program/06E.ASCII Table.c
@@ -3,14 +3,13 @@
 
 int main()
 {
-    int No= 0;
+        int No = 0;
 
-    for(No =0 ;No < 128;No++)
+        for(No = 0 ;No < 128;No++)
+        {
+          printf("\n ASCII Table %d = %c",No,No);
+        }
 
-
-    printf("\n ASCII Table %d = %c",No,No);
-
-
-    getch();
-    return 0;
+        getch();
+        return 0;
 }
