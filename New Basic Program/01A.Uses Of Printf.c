d716c62d73fb51679ba0b46a8f0a4bcd3ea098fe
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-22 22:48:08 +0530

    Modified Program

--- New Basic Program/01A.Uses Of Printf.c
+++ New Basic Program/01A.Uses Of Printf.c
@@ -3,6 +3,7 @@
 
 int main()
 {
+
     printf("\n %-5d %-10s %-3d %3d %d %3f",255,"Amit",07,81,150,50.00);
 
     printf("\n %-5d %-10s %-3d %3d %d %3f",02,"Raj",70,80,200,67.55);
@@ -10,8 +11,6 @@ int main()
     printf("\n %-5d %-10s %-3d %3d %d %3f",150,"Ajay",82,18,170,56.40);
 
 
-
-    
     getch();
     return 0;
 }
