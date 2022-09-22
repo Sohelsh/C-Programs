65bd9b1c3b4b59ff634e49d06f84f3cd8cdf9c7d
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-22 22:49:12 +0530

    Modified Progrma

--- New Basic Program/02B.Accept Student Information Print.c
+++ New Basic Program/02B.Accept Student Information Print.c
@@ -4,38 +4,38 @@
 
 int main()
 {
-    char Stdname[20]= {'\0'};
-    int Roll_No=0;
-    char Course[10]={'\0'};
-    char City[10]={'\0'};
+        char Stdname[20]= {'\0'};
+        int Roll_No=0;
+        char Course[10]={'\0'};
+        char City[10]={'\0'};
 
 
-    printf("\n Enter Student Name = ");
-    gets(Stdname);
+        printf("\n Enter Student Name = ");
+        gets(Stdname);
 
-    printf("\n Enter Student Roll No = ");
-    scanf("%d",&Roll_No);
+        printf("\n Enter Student Roll No = ");
+        scanf("%d",&Roll_No);
 
-    fflush(stdin);
+        fflush(stdin);
 
-    printf("\n Enter Student Course Name =");
-    gets(Course);
+        printf("\n Enter Student Course Name =");
+        gets(Course);
 
-    printf("\n Enter Student City Name = ");
-    gets(City);
+        printf("\n Enter Student City Name = ");
+        gets(City);
 
-    printf("\n ====================***********===================");
+        printf("\n ====================***********===================");
 
-    printf("\n\n Student Name = %s",Stdname);
-    printf("\n\n Student Roll No =%d ",Roll_No);
-    printf("\n\n Student Course Name = %s",Course);
-    printf("\n\n Student City Name =%s ",City);
+        printf("\n\n Student Name = %s",Stdname);
+        printf("\n\n Student Roll No =%d ",Roll_No);
+        printf("\n\n Student Course Name = %s",Course);
+        printf("\n\n Student City Name =%s ",City);
 
-    printf("\n ====================***********===================");
+        printf("\n ====================***********===================");
 
 
-    getch();
-    return 0;
+        getch();
+        return 0;
 
 
 
