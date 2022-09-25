7a6c9aaac580dab2482b5ea4c492c2706d6c1323
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-25 23:38:34 +0530

    Modified File

--- Conditional Statements & Loops/03C.Maximum From 2  Number (Use ternary operator).c
+++ Conditional Statements & Loops/03C.Maximum From 2  Number (Use ternary operator).c
@@ -3,25 +3,17 @@
 
 int main()
 {
-  int Num1 = 0, Num2 =  0;
-  
-  printf("\n Enter 2 Integer Numbers = ");
-  scanf("%d%d",&Num1,&Num2);
-  
-  
-  (Num1 == Num2)? printf("\n Both Givan Number are Equal "): (Num1 > Num2)? printf("\n Number %d is Maximum.",Num1): printf("\n Number %d is Maximum.",Num2);
-  
-  
-  printf("\n Thanks...!!");
-  
-  getch();
-  return 0;
-  
-  
-  
-  
-  
-  
-  getch();
-  return 0;
+        int Num1 = 0, Num2 =  0;
+
+        printf("\n Enter 2 Integer Numbers = ");
+        scanf("%d%d",&Num1,&Num2);
+
+
+        (Num1 == Num2)? printf("\n Both Givan Number are Equal "): (Num1 > Num2)? printf("\n Number %d is Maximum.",Num1): printf("\n Number %d is Maximum.",Num2);
+
+
+        printf("\n Thanks...!!");
+
+        getch();
+        return 0;
 }
