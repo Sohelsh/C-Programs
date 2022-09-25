f94660fef5e5604be1c7c373b9083b8dd52045ac
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-25 23:41:43 +0530

    Modified File

--- Conditional Statements & Loops/06F.Program to Find Inputted Number is Divisible by both 5 & 7 or only by 5 or only 7 or not by both 5 & 7.c
+++ Conditional Statements & Loops/06F.Program to Find Inputted Number is Divisible by both 5 & 7 or only by 5 or only 7 or not by both 5 & 7.c
@@ -3,29 +3,29 @@
 
 int main()
 {
-   int Num = 0;
-   
-   printf("\n\n Enter a Number = ");
-   scanf("%d",&Num);
-   
-   if( (Num % 5 == 0) && (Num % 7 == 0) )
-    {
-      printf("\n Numbar is Divisibal By Both 5 & 7.");
-    }
-    else if (Num % 5 == 0)
-    {
-      printf("\n Number is Only Divisibal By 5 ");
-    }
-    else if( Num % 7 == 0)
-    {
-      printf("\n Number is Only Divisibal By 7 ");
-    }
-    else 
-    {
-      printf("\n Number is Not Divisibal By Both 5 & 7");
-    }
-    printf("\n\n Thanks ");
-    
-    getch();
-    return 0 ;
+        int Num = 0;
+
+        printf("\n\n Enter a Number = ");
+        scanf("%d",&Num);
+
+        if( (Num % 5 == 0) && (Num % 7 == 0) )
+        {
+            printf("\n Numbar is Divisibal By Both 5 & 7.");
+        }
+        else if (Num % 5 == 0)
+        {
+            printf("\n Number is Only Divisibal By 5 ");
+        }
+        else if( Num % 7 == 0)
+        {
+            printf("\n Number is Only Divisibal By 7 ");
+        }
+        else
+        {
+            printf("\n Number is Not Divisibal By Both 5 & 7");
+        }
+        printf("\n\n Thanks ");
+
+        getch();
+        return 0 ;
 }
