eb52c441670586a9f3d7ac55d62fdc199ea18123
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-25 23:39:41 +0530

    Modified File

--- Conditional Statements & Loops/04D.Maximum From 2 Numbers  Using if else.c
+++ Conditional Statements & Loops/04D.Maximum From 2 Numbers  Using if else.c
@@ -3,27 +3,27 @@
 
 int main()
 {
-  int Num1 = 0 ,Num2 =  0;
-  
-  printf("\n Enter 2 Integers Numbers = ");
-  scanf("%d%d",Num1,Num2);
-  
-  if(Num1 == Num2)
-  {
-    printf("\n Both Given Number are Equal.");
-  }
-  else if (Num1 > Num2)
-  {
-    printf("\n Number %d is Maximum .",Num1);
-  }
-  else
-  {
-    printf("\n Number %d is Maximum.",Num2);
-  }
-  
-  printf("\n Thanks..!!");
-  
-  getch();
-  return 0;
-  
+        int Num1 = 0 ,Num2 =  0;
+
+        printf("\n Enter 2 Integers Numbers = ");
+        scanf("%d%d",Num1,Num2);
+
+        if(Num1 == Num2)
+        {
+            printf("\n Both Given Number are Equal.");
+        }
+        else if (Num1 > Num2)
+        {
+            printf("\n Number %d is Maximum .",Num1);
+        }
+        else
+        {
+            printf("\n Number %d is Maximum.",Num2);
+        }
+
+        printf("\n Thanks..!!");
+
+        getch();
+        return 0;
+
 }
