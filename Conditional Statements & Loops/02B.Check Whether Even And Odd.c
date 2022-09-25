b6d9c4eb0cd47701fbb8eb32ebd0f97d7cf037e9
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-25 23:36:53 +0530

    Modified File

--- Conditional Statements & Loops/02B.Check Whether Even And Odd.c
+++ Conditional Statements & Loops/02B.Check Whether Even And Odd.c
@@ -4,24 +4,25 @@
 
 int main()
 {
-  int Num =  0;
-   
-   printf("\n Enter an Integer Number => ");
-   scanf("%d",&Num);
-   
-   if(Num == 0)
-     {
-        printf("\n Given Number is Natural.");
-     }
-    else if (Num % 2 == 0)
-     {
-        printf("\n Given Number is Even.");
-     }
-    else
-     {
-         printf("\n Given Number is Odd.");
-     }
-  return 0;
-  getch();
-  
+        int Num =  0;
+
+        printf("\n Enter an Integer Number => ");
+        scanf("%d",&Num);
+
+        if(Num == 0)
+        {
+            printf("\n Given Number is Natural.");
+        }
+        else if (Num % 2 == 0)
+        {
+            printf("\n Given Number is Even.");
+        }
+        else
+        {
+            printf("\n Given Number is Odd.");
+        }
+
+        return 0;
+        getch();
+
  }
