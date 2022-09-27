9fd627aa6f456a21f6b969fa9f5319ceaa3ddccb
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-27 23:45:15 +0530

    Modified Program

--- Practise Problem Of C programing/01.Structure.c
+++ Practise Problem Of C programing/01.Structure.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<conio.h>
+
  struct Workshop
  {
      char Std_Name[8];
@@ -7,25 +8,26 @@
      int Std_Fee;
  };
  int main()
- {    int i =0;
-      struct Workshop wor[3];
+{
+        int i = 0;
+        struct Workshop wor[3];
 
-     for(i = 1;i <= 3 ;i++)
-     {
-         printf("\n Entar Student Name = ");
-         scanf("%s",&wor[i].Std_Name);
+        for(i = 1;i <= 3 ;i++)
+        {
+            printf("\n Entar Student Name = ");
+            scanf("%s",&wor[i].Std_Name);
 
-         printf("\n Entar Student Cours = ");
-         scanf("%s",&wor[i].Std_Cours);
+            printf("\n Entar Student Cours = ");
+            scanf("%s",&wor[i].Std_Cours);
 
-         printf("\n Entar Student Fee = ");
-         scanf("%d",&wor[i].Std_Fee);
-     }
-    for(i = 1;i < 3 ;i++)
-    {
-           printf("\n first Student information =%s %s %d",wor[i].Std_Name,wor[i].Std_Cours,wor[i].Std_Fee);
+            printf("\n Entar Student Fee = ");
+            scanf("%d",&wor[i].Std_Fee);
+        }
+        for(i = 1;i < 3 ;i++)
+        {
+            printf("\n first Student information =%s %s %d",wor[i].Std_Name,wor[i].Std_Cours,wor[i].Std_Fee);
 
-    }
+        }
 
 
  }
