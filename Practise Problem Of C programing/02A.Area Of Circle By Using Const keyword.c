a1e75990c2df61764a322193e857d1f8586a4fb7
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-27 23:46:22 +0530

    Modified Program

--- Practise Problem Of C programing/02A.Area Of Circle By Using Const keyword.c
+++ Practise Problem Of C programing/02A.Area Of Circle By Using Const keyword.c
@@ -4,17 +4,17 @@
 
 int main()
 {
-  const pi = 3.14;
-  int R = 0;
-  float Area = 0.0;
-  
-  printf("\n Enter A Radious = ");
-  scanf("%d",&R);
-  
-  Area = pi * R * R;
-  
-  printf("\n Area Of Circal =%f",Area);
-  
-  getch();
-  return 0;
+        const pi = 3.14;
+        int R = 0;
+        float Area = 0.0;
+
+        printf("\n Enter A Radious = ");
+        scanf("%d",&R);
+
+        Area = pi * R * R;
+
+        printf("\n Area Of Circal =%f",Area);
+
+        getch();
+        return 0;
 }
