4c3fc1af49ddc931bc622c4ad3d631368ce27534
Sohelsh <sohelshaikhadam@gmail.com>
2022-09-27 23:37:41 +0530

    Modified Program

--- Practise Problem Of C programing/01.ACount A Any Digit.c
+++ Practise Problem Of C programing/01.ACount A Any Digit.c
@@ -5,27 +5,27 @@
 
 int main()
 {
-  int No= 0,Temp =0,Dig = 0,CountNum=0,Cnt =0;
-  
-  printf("\n Enter A Number = ");
-  scanf("%d",&No);
-  
-  Temp = No;
-  
-  printf("\n Enter Count Number = ");
-  scanf("%d",&CountNum);
-  
-  while( Temp != 0)
-  {
-    Dig = Temp %10;
-    Temp= Temp /10;
-    
-    if(Dig == CountNum)
-    {
-      Cnt++;
-    }
-  }
-  
-  printf("\n Number of Count %d",Cnt);
-  return 0;
+        int No = 0,Temp = 0,Dig = 0,CountNum = 0,Cnt = 0;
+
+        printf("\n Enter A Number = ");
+        scanf("%d",&No);
+
+        Temp = No;
+
+        printf("\n Enter Count Number = ");
+        scanf("%d",&CountNum);
+
+        while( Temp != 0)
+        {
+            Dig = Temp %10;
+            Temp= Temp /10;
+
+            if(Dig == CountNum)
+            {
+                Cnt++;
+            }
+        }
+
+        printf("\n Number of Count %d",Cnt);
+        return 0;
 }
