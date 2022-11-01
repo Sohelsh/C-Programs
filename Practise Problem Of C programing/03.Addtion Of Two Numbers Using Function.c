4fc83bd4f6952a510e9b372e431490d28e9e4c9e
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-01 23:05:58 +0530

    Modified Program

--- Practise Problem Of C programing/03.Addtion Of Two Numbers Using Function.c
+++ Practise Problem Of C programing/03.Addtion Of Two Numbers Using Function.c
@@ -1,26 +1,26 @@
 #include<stdio.h>
 #include<conio.h>
- int Add(int ,int);
 
- int main()
+int Add(int ,int);
+
+int main()
  {
-      int No1 = 0, No2 = 0;
+            int No1 = 0, No2 = 0;
 
-      printf("\n Entar Two Numbers = ");
-      scanf("%d%d",&No1,&No2);
+            printf("\n Entar Two Numbers = ");
+            scanf("%d%d",&No1,&No2);
 
-      Add(No1,No2);
-
-
+            Add(No1,No2);
  }
- int Add(int N1,int N2)
+
+int Add(int N1,int N2)
  {
-      int Sum = 0;
+            int Sum = 0;
 
-      Sum = N1 + N2;
+            Sum = N1 + N2;
 
-      printf("%d",Sum);
+            printf("%d",Sum);
 
-      getch();
-      return 0;
+            getch();
+            return 0;
  }
