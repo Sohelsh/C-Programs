7d0a3b59337d52a148911c32a5bc116e083b383d
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-01 23:06:58 +0530

    Modified Program

--- Practise Problem Of C programing/04E. Program of the voters are eligible or not eligible.c
+++ Practise Problem Of C programing/04E. Program of the voters are eligible or not eligible.c
@@ -4,21 +4,21 @@
 //Compiler version gcc  6.3.0
 
 int main()
-{ 
-  int Age = 0;
-  
-  printf("\n\t Enter A Age = ");
-  scanf("%d",&Age);
-  
-  if(Age < 18)
-  {
-    printf("\n Voter Is Not Eligible For Voting = ");
-  }
-  else
-   {
-     printf("\n Voter Is Eligible For voting= ");
-   }
-   
-  getch();
-  return 0;
+{
+            int Age = 0;
+
+            printf("\n\t Enter A Age = ");
+            scanf("%d",&Age);
+
+            if(Age < 18)
+            {
+            printf("\n Voter Is Not Eligible For Voting = ");
+            }
+            else
+            {
+            printf("\n Voter Is Eligible For voting= ");
+            }
+
+            getch();
+            return 0;
 }
