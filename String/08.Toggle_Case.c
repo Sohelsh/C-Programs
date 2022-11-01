ce4b644aea00c58814a6fafa58bf28620a808de9
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-01 23:00:59 +0530

    Added File

---
+++ String/08.Toggle_Case.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[40] = {'\0'};
+    int i = 0;
+
+    puts("\n Enter The String");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] >= 'A' && cSrc[i] <= 'Z')
+        {
+            cSrc[i] = cSrc[i] + 32;
+        }
+        else if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+        {
+            cSrc[i] = cSrc[i] - 32;
+        }
+        i++;
+    }
+
+    printf("\n Toggle case in given string is %s",cSrc);
+
+    getch();
+    return 0;
+}
