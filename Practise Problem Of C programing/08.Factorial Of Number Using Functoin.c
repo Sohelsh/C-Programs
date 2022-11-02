27229ef2cc6a0545c49610781bb051337f281bff
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-02 23:42:35 +0530

    Added Program

--- Practise Problem Of C programing/08.Factorial Of Number Using Functoin.c
+++ Practise Problem Of C programing/08.Factorial Of Number Using Functoin.c
@@ -1,32 +1,34 @@
 #include<stdio.h>
 #include<conio.h>
 
- long long int Factorial_Of_Numbar(int);
+long long int Factorial_Of_Numbar(int);
 
- int main()
- {
-      int No =0;
-      long long int Res =0;
+int main()
+{
+            int No = 0;
+            long long int Res = 0;
 
-      printf("\n Entar A Number = ");
-      scanf("%d",&No);
+            printf("\n Entar A Number = ");
+            scanf("%d",&No);
 
-      Res = Factorial_Of_Numbar(No);
+            Res = Factorial_Of_Numbar(No);
 
-      printf("\n Factorial Of Number %d = %lld ",No,Res);
+            printf("\n Factorial Of Number %d = %lld ",No,Res);
 
-      getch();
- }
-   long long int Factorial_Of_Numbar(Num)
-  {
-       long long int Fact = 1;
+            getch();
+            return 0;
+}
 
-       while(Num != 0)
-       {
-            Fact = Fact * Num;
+long long int Factorial_Of_Numbar(Num)
+{
+            long long int Fact = 1;
 
-            Num--;
-       }
+            while(Num != 0)
+            {
+                Fact = Fact * Num;
 
-       return Fact;
-  }
+                Num--;
+            }S
+
+            return Fact;
+}
