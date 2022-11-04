fb4180fbfb59ed3303d3ba054f3396c02e4cbe4e
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-04 22:54:25 +0530

    Added File

--- Practise Problem Of C programing/11.Program Of Convert To Binary To Desimal .c
+++ Practise Problem Of C programing/11.Program Of Convert To Binary To Desimal .c
@@ -1,23 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
 
- int main()
- {
-       int No = 0,Dig = 0,Des = 0,Base = 1;
+int main()
+{
+            int No = 0,Dig = 0,Des = 0,Base = 1;
 
-      printf("\n Entar A Number = ");
-      scanf("%d",&No);
+            printf("\n Entar A Number = ");
+            scanf("%d",&No);
 
-      while(No != 0)
-      {
-           Dig = No %10;
-           Des = Des+Dig*Base;
-           No = No/10;
-           Base = Base *2;
-      }
+            while(No != 0)
+            {
+                    Dig = No %10;
+                    Des = Des+Dig*Base;
+                    No = No/10;
+                    Base = Base *2;
+            }
 
-       printf("\n %d",Des);
+            printf("\n %d",Des);
 
-       getch();
-       return 0;
- }
+            getch();
+            return 0;
+
+}
