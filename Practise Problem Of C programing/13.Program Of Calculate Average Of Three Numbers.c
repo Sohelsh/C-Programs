29da799ba67b19782e14f6e2c1a7bd2a65269ff7
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-04 22:55:17 +0530

    Added File

--- Practise Problem Of C programing/13.Program Of Calculate Average Of Three Numbers.c
+++ Practise Problem Of C programing/13.Program Of Calculate Average Of Three Numbers.c
@@ -3,16 +3,16 @@
 
  int main()
  {
-      int N1 = 0,N2 = 0,N3 = 0;
-      float Ave = 0.0;
+            int N1 = 0,N2 = 0,N3 = 0;
+            float Ave = 0.0;
 
-      printf("\n Entar Three Numbers = ");
-      scanf("%d%d%d",&N1,&N2,&N3);
+            printf("\n Enter Three Numbers = ");
+            scanf("%d%d%d",&N1,&N2,&N3);
 
-      Ave = (N1+N2+N3)/3;
+            Ave = (N1+N2+N3)/3;
 
-      printf("\n Average Of Three Numbers %0.2f",Ave);
+            printf("\n Average Of Three Numbers %0.2f",Ave);
 
-      getch();
-      return 0;
+            getch();
+            return 0;
  }
