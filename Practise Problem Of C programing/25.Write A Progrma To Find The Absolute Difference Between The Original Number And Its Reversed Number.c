adfe8733e9a3fd6d8394a6ff19ec5479e7f7f66e
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-05 23:59:15 +0530

    Added Program

--- Practise Problem Of C programing/25.Write A Progrma To Find The Absolute Difference Between The Original Number And Its Reversed Number.c
+++ Practise Problem Of C programing/25.Write A Progrma To Find The Absolute Difference Between The Original Number And Its Reversed Number.c
@@ -3,27 +3,27 @@
 
 int Reverse_Diff(int Num)
 {
-    int Dig = 0, Temp = 0,Rev = 0;
+        int Dig = 0, Temp = 0,Rev = 0;
 
-    Temp = Num;
+        Temp = Num;
 
-    while(Temp != 0)
-    {
-        Dig  = Temp % 10;
-        Temp = Temp / 10;
-        Rev  = (Rev * 10) + Dig;
-    }
-    return Num - Rev;
+        while(Temp != 0)
+        {
+            Dig  = Temp % 10;
+            Temp = Temp / 10;
+            Rev  = (Rev * 10) + Dig;
+        }
+        return Num - Rev;
 }
 int main()
 {
-    int No = 0;
+        int No = 0;
 
-    printf("\n Enter Number = ");
-    scanf("%d",&No);
+        printf("\n Enter Number = ");
+        scanf("%d",&No);
 
-    printf("\n Difference Of Original Number And Reversed Number = %d",Reverse_Diff(No));
+        printf("\n Difference Of Original Number And Reversed Number = %d",Reverse_Diff(No));
 
-    getch();
-    return 0;
+        getch();
+        return 0;
 }
