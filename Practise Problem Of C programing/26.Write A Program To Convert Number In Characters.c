02c33cf86b86181ad80e98246b19dcd02577a362
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-06 23:37:42 +0545

    Added File

--- Practise Problem Of C programing/26.Write A Program To Convert Number In Characters.c
+++ Practise Problem Of C programing/26.Write A Program To Convert Number In Characters.c
@@ -4,73 +4,72 @@
 
 int main()
 {
-    int No = 0,Dig = 0,Rev = 0;
+        int No = 0,Dig = 0,Rev = 0;
 
-    printf("\n Enter Number = ");
-    scanf("%d",&No);
+        printf("\n Enter Number = ");
+        scanf("%d",&No);
 
-    while(No > 0)
-    {
-        Dig = No % 10;
-        Rev = (Rev * 10) + Dig;
-        No = No/ 10;
-    }
-    No = Rev;
-
-    while(No > 0)
-    {
-        Dig = No % 10;
-
-        switch(Dig)
+        while(No > 0)
         {
-           case 1:
-                  printf(" One ");
-                  break;
+            Dig = No % 10;
+            Rev = (Rev * 10) + Dig;
+            No = No/ 10;
+        }
+        No = Rev;
 
-           case 2:
-                  printf("Two ");
-                  break;
+        while(No > 0)
+        {
+            Dig = No % 10;
 
-           case 3:
-                  printf("Three ");
-                  break;
+            switch(Dig)
+            {
+               case 1:
+                      printf(" One ");
+                      break;
 
-           case 4:
-                  printf("Four");
-                  break;
+               case 2:
+                      printf("Two ");
+                      break;
 
-           case 5:
-                  printf("Five");
-                  break;
+               case 3:
+                      printf("Three ");
+                      break;
 
-           case 6:
-                  printf("Six");
-                  break;
+               case 4:
+                      printf("Four");
+                      break;
 
-           case 7:
-                  printf("Seven");
-                  break;
+               case 5:
+                      printf("Five");
+                      break;
 
-           case 8:
-                  printf("Eight");
-                  break;
+               case 6:
+                      printf("Six");
+                      break;
 
-           case 9:
-                  printf("Nine");
-                  break;
+               case 7:
+                      printf("Seven");
+                      break;
 
-           case 0:
-                  printf("Zero");
-                  break;
+               case 8:
+                      printf("Eight");
+                      break;
+
+               case 9:
+                      printf("Nine");
+                      break;
+
+               case 0:
+                      printf("Zero");
+                      break;
+
+            }
+
+            No = No / 10;
 
         }
 
-        No = No / 10;
-
-    }
-
-
-    getch();
-    return 0;
+        getch();
+        return 0;
 }
 
