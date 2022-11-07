d5997674ab0cf5b527866b1a050b1310688e84a2
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-07 23:32:50 +0545

    Modified Program

--- String/06.Count of Digit In String.c
+++ String/06.Count of Digit In String.c
@@ -5,24 +5,24 @@
 
 int main()
 {
-  char cSrc[40]={'\0'};
-  int i = 0,D_cnt =0;
-  
-  printf("Enter A String = ");
-  gets(cSrc);
-  
-  while(cSrc[i] != '\0')
-  {
-    if(cSrc[i] >= '0'&& cSrc[i] <= '9')
-    {
-      D_cnt++;
-    }
-    i++;
-  }
-   printf("\n Count Of Digits In String : %d",D_cnt);
-  
-  
-  getch();
-  return 0;
-  
+        char cSrc[40] = {'\0'};
+        int i = 0,D_cnt =0;
+
+        printf("Enter A String = ");
+        gets(cSrc);
+
+        while(cSrc[i] != '\0')
+        {
+            if(cSrc[i] >= '0'&& cSrc[i] <= '9')
+            {
+              D_cnt++;
+            }
+            i++;
+        }
+        printf("\n Count Of Digits In String : %d",D_cnt);
+
+
+        getch();
+        return 0;
+
 }
