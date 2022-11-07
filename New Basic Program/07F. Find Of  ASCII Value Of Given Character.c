d3ca5c9b0e4da0036cbaadccefeb40d5dc1e4d55
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-07 23:43:45 +0545

    Modified Program

--- New Basic Program/07F. Find Of  ASCII Value Of Given Character.c
+++ New Basic Program/07F. Find Of  ASCII Value Of Given Character.c
@@ -3,18 +3,14 @@
 
 int main()
 {
-    char Ch= 0;
-
+    char Ch = 0;
 
     printf("\n Character to ASCII Conversion  \n");
 
     printf("\n Enter A character = ");
     scanf("%c",&Ch);
 
-
-
     printf("\n ASCII value Of Charcater %c = %d",Ch,Ch);
-
 
     getch();
     return 0;
