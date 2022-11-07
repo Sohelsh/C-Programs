8ef97116925b95e404eac8ea73400f42e33e9079
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-07 23:33:35 +0545

    Modified Program

--- String/07. Copy String.c
+++ String/07. Copy String.c
@@ -5,27 +5,27 @@
 
 int main()
 {
-   char cSrc[30] = {'\0'};
-   char copy_s[20]= {};
-   int i =0;
-   
-  printf("Enter A String =  ");
-  gets(cSrc);
-  
-  while( cSrc[i] != '\0')
-      {
-        copy_s[i] = cSrc[i];
-        
-        i++;
-      }
-       copy_s[i] = '\0';
-      
-      
-      printf("\n Given A String Is %s",cSrc);
-      
-      printf("\n Copy String Is %s",copy_s);
-  
-       
-       getch();
-      return 0;
+        char cSrc[30] = {'\0'};
+        char copy_s[20]= {};
+        int i =0;
+
+        printf("Enter A String =  ");
+        gets(cSrc);
+
+        while( cSrc[i] != '\0')
+        {
+            copy_s[i] = cSrc[i];
+
+            i++;
+        }
+        copy_s[i] = '\0';
+
+
+        printf("\n Given A String Is %s",cSrc);
+
+        printf("\n Copy String Is %s",copy_s);
+
+
+        getch();
+        return 0;
 }
