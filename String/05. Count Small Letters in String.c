f77c43c2c267ed3de4d2a984fd430600a6afef7d
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-07 23:31:53 +0545

    Modified Program

--- String/05. Count Small Letters in String.c
+++ String/05. Count Small Letters in String.c
@@ -4,26 +4,26 @@
   // Count Small Letters In String
 
 int main()
-{  
-   char cSrc[20]= {'\0'};
-   int i =0,L_cnt = 0;
-   
-     printf("Enter A String = ");
-     gets(cSrc);
-     
-     while( cSrc[i] != '\0')
-     {
-       if( cSrc[i] >= 'a' && cSrc[i] <= 'z')
-       {
-          L_cnt++;
-       
-       }
-       i++;
-     }
-     printf("\n Count Small Letters is  :%d",L_cnt);
- 
-     getch();
-     return 0;
-  
-  
+{
+         char cSrc[20] = {'\0'};
+         int i =0,L_cnt = 0;
+
+         printf("Enter A String = ");
+         gets(cSrc);
+
+         while( cSrc[i] != '\0')
+         {
+           if( cSrc[i] >= 'a' && cSrc[i] <= 'z')
+           {
+              L_cnt++;
+
+           }
+           i++;
+         }
+         printf("\n Count Small Letters is  :%d",L_cnt);
+
+         getch();
+         return 0;
+
+
 }
