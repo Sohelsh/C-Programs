1d177d8029f922684e2c5099bc1416bcb89bcd43
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-09 00:08:41 +0545

    Modified Program

--- Pattern Program/03.Pattern Printing.c
+++ Pattern Program/03.Pattern Printing.c
@@ -3,31 +3,29 @@
 
 int main()
 {
-  int i = 0,j = 0;
-  
-   printf("\n\n Pattren is => \n\n");
-    
-    for(i = 1;i <= 7; i++)
-     {
-       for(j = 1;j <= 1; j++)
-       {
-             if(i == 1||i == 7||j == 1||j == 3)
+        int i = 0,j = 0;
+
+        printf("\n\n Pattren is => \n\n");
+
+        for(i = 1;i <= 7; i++)
+        {
+            for(j = 1;j <= 1; j++)
             {
-              printf(" * ");
+                if(i == 1||i == 7||j == 1||j == 3)
+                {
+                   printf(" * ");
+                }
+                else
+                {
+                   printf("   ");
+                }
+
             }
-            else
-            {
-              printf("   ");
-            } 
-          
+            printf("\n");
         }
-        printf("\n");
-        
-           
-      }
-       
-       printf("\n\n Thanks");
-       
-     getch();
-     return 0;
+
+        printf("\n\n Thanks");
+
+        getch();
+        return 0;
 }
