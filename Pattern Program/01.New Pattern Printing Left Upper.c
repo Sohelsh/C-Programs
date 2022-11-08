c1be6b77da6e969b8202086210629b0c737d2fe7
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-09 00:07:04 +0545

    Modified Program

--- Pattern Program/01.New Pattern Printing Left Upper.c
+++ Pattern Program/01.New Pattern Printing Left Upper.c
@@ -3,31 +3,29 @@
 
 int main()
 {
+        int R = 0,C = 0 ,N = 0;
 
-    int R = 0,C = 0 ,N =0;
+        printf("\n Enter Row and Column Size =");
+        scanf("%d",&N);
 
-    printf("\n Enter Row and Column Size =");
-    scanf("%d",&N);
-    
-    for(R = 1;R <= N ; R++)
-    {
-      for(C =1;C <= N ; C++)
-      {
-        if(R+C <= N + 1)
+        for(R = 1;R <= N ; R++)
         {
-          printf(" * ");
-         
-         }
-        else
-        {
-          printf("   ");
+            for(C =1;C <= N ; C++)
+            {
+                if(R+C <= N + 1)
+                {
+                    printf(" * ");
+
+                }
+                else
+                {
+                    printf("   ");
+                }
+
+            }
+            printf("\n");
         }
-        
-      }
-      printf("\n");
-    }
-    
-    
-    getch();
-    return 0;
+
+        getch();
+        return 0;
  }
