3b5bc5e6dbd8bdef1a77cdce116854f335dbd810
Sohelsh <sohelshaikhadam@gmail.com>
2022-11-09 11:16:08 +0545

    Modified Program

--- Pattern Program/10.New Pattern Printing H.c
+++ Pattern Program/10.New Pattern Printing H.c
@@ -5,24 +5,27 @@
 
 int main()
 {
-   int R = 0,C = 0 ,N =0;
-   
-   printf("\n Entar Pattern Size =");
-   scanf("%d",&N);
-  
-  for(R =1 ;R <= N; R++)
-   {
-     for(C = 1; C<= N; C++)
-     {
-       if(C == 1|| C == N|| R == (N/2)+1)
-       {
-         printf(" * ");
-       }
-       else
+        int R = 0,C = 0 ,N = 0;
+
+        printf("\n Entar Pattern Size =");
+        scanf("%d",&N);
+
+        for(R =1 ;R <= N; R++)
         {
-          printf("   ");
+            for(C = 1; C<= N; C++)
+            {
+                if(C == 1|| C == N|| R == (N/2)+1)
+                {
+                    printf(" * ");
+                }
+                else
+                {
+                    printf("   ");
+                }
+            }
+            printf("\n");
         }
-     }
-     printf("\n");
-   }
-  }
+
+        getch();
+        return 0;
+}
