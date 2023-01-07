3178a89079821ccde7a9c7e912ecf07726215f65
Sohelsh <sohelshaikhadam@gmail.com>
2023-01-07 23:48:50 +0530

    Program Added

--- Practise Problem Of C programing/02B.Area Of Circle By Using define.c
+++ Practise Problem Of C programing/02B.Area Of Circle By Using define.c
@@ -5,16 +5,16 @@
 
 int main()
 {
-  int r; float area ;
-  
-  printf("\n Enter of Rodious = ");
-  scanf("%d",&r);
-  
-  area = pi * r * r;
-  
-  printf("\n area of Circle  = %f",area);
-  
-  
-  getch();
-  return 0;
+        int r; float area ;
+
+        printf("\n Enter of Rodious = ");
+        scanf("%d",&r);
+
+        area = pi * r * r;
+
+        printf("\n area of Circle  = %f",area);
+
+
+        getch();
+        return 0;
 }
