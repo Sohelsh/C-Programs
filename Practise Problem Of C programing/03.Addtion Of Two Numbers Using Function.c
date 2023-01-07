2ad461c8d83c9dd34e0e68acf4954aa6df9242dd
Sohelsh <sohelshaikhadam@gmail.com>
2023-01-07 23:50:52 +0530

    Program Added

--- Practise Problem Of C programing/03.Addtion Of Two Numbers Using Function.c
+++ Practise Problem Of C programing/03.Addtion Of Two Numbers Using Function.c
@@ -7,7 +7,7 @@ int main()
  {
             int No1 = 0, No2 = 0;
 
-            printf("\n Entar Two Numbers = ");
+            printf("\n Enter Two Numbers = ");
             scanf("%d%d",&No1,&No2);
 
             Add(No1,No2);
