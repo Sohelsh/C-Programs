c78185d0352e4e7e32cb60d02dc70236c0a80459
Sohelsh <sohelshaikhadam@gmail.com>
2023-01-11 20:51:06 +0530

    Practice

---
+++ Practise Problem Of C programing/27.Test-1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+#define MUL5(X) X*5
+
+int main()
+{
+   /* int a,b = 2,c= 5;
+
+    a=(b,++c,b+c);
+
+    printf("%d",a);*/
+
+   /*  int a = 0,b = 1;
+
+     if(a && ++b);
+
+     printf("%d",b);*/
+
+     int y;
+     y=MUL5(2+4);
+
+     printf("%d",y);
+
+    getch();
+    return 0;
+}
