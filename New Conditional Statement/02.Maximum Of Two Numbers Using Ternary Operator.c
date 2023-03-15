d094160d502f76bd12da425abe372f55ee27c07d
Sohelsh <sohelshaikhadam@gmail.com>
2023-03-15 22:39:29 +0530

    Practice

--- New Conditional Statement/02.Maximum Of Two Numbers Using Ternary Operator.c
+++ New Conditional Statement/02.Maximum Of Two Numbers Using Ternary Operator.c
@@ -4,12 +4,12 @@
 
 int main()
 {
-    int No1 =0, No2 = 0;
+    int No1 = 0, No2 = 0;
 
-    printf("\n Enter A Numbars = ");
+    printf("\n Enter A Numbers = ");
     scanf("%d%d",&No1,&No2);
 
-    (No1 > No2)?printf("\n First Numbar is Maximum "):printf("\n Second Numbar Is MAximum ");
+    (No1 > No2)?printf("\n First Number is Maximum "):printf("\n Second Number Is Maximum ");
 
     getch();
     return 0;
