220bba6294fdc41e633e8899808e314b5501b4b4
Sohelsh <sohelshaikhadam@gmail.com>
2023-03-16 23:09:28 +0530

    Practice

--- New Conditional Statement/05. Maximum of 3 Numbers.c
+++ New Conditional Statement/05. Maximum of 3 Numbers.c
@@ -6,29 +6,29 @@
   {
       int Num1 = 0,Num2 = 0,Num3 = 0;
 
-      printf("\n Enter First Numbar = ");
+      printf("\n Enter First Number = ");
       scanf("%d",&Num1);
 
-      printf("\n Enter Second Numbar = ");
+      printf("\n Enter Second Number = ");
       scanf("%d",&Num2);
 
-      printf("\n Enter Third Numbar = ");
+      printf("\n Enter Third Number = ");
       scanf("%d",&Num3);
 
 
       if( Num1 >= Num2 && Num1 >= Num3 )
       {
-          printf("\n First Numbar Is Maximum = %d",Num1);
+          printf("\n First Number Is Maximum = %d",Num1);
       }
 
       else if(Num2 >= Num1 && Num2 >= Num3)
       {
-          printf("\n Second Numbar Is Maximum = %d",Num2);
+          printf("\n Second Number Is Maximum = %d",Num2);
       }
 
       else
       {
-          printf("\n Third Numbar Is Maximum = %d",Num3);
+          printf("\n Third Number Is Maximum = %d",Num3);
       }
 
       getch();
