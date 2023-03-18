1440e29db99aaaa2d42a6381873e593f48455743
Sohelsh <sohelshaikhadam@gmail.com>
2023-03-18 23:30:24 +0530

    Practice

--- New Digit Program/08.Power No1 Raise To No2.c
+++ New Digit Program/08.Power No1 Raise To No2.c
@@ -5,7 +5,7 @@
  {
       int No1 = 0,No2 = 0,Pow = 0,i = 0;
 
-      printf("\n Entar 2 Numbar To Find Num1 Raise To Num2 = ");
+      printf("\n Enter 2 Number To Find Num1 Raise To Num2 = ");
       scanf("%d%d",&No1,&No2);
 
       for(i = 1,Pow = 1;i <= No2;i++)
