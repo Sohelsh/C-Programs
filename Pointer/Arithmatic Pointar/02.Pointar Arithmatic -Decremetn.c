c67acf470a183f7b80c501d02cfe816f50242c10
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-03-28 23:21:50 +0530

    Practice

--- Pointer/Arithmatic Pointar/02.Pointar Arithmatic -Decremetn.c
+++ Pointer/Arithmatic Pointar/02.Pointar Arithmatic -Decremetn.c
@@ -6,17 +6,17 @@
      int Num[5]={31,66,79,22,57};
      int *ptr = &Num[2];
 
-     printf("\n Base Addres Of Array = %d",Num);
+     printf("\n Base Address Of Array = %d",Num);
      printf("\n Value Of Pointer = %d",ptr);
-     printf("\n Value Of Array Pointer Poninting = %d",*ptr);
+     printf("\n Value Of Array Pointer Pointing = %d",*ptr);
 
      getch();
 
      ptr--;
 
 
-     printf("\n Aftar Decrement Value Of Pointar = %d",ptr);
-     printf("\n Value Of Array Pointar Pointing = %d",*ptr);
+     printf("\n Aftar Decrement Value Of Pointer = %d",ptr);
+     printf("\n Value Of Array Pointer Pointing = %d",*ptr);
 
      getch();
      return 0;
