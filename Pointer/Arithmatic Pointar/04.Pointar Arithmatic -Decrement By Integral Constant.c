620cd91975eb895407e4fa4a2ae623de7982d7cb
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-03-28 23:22:34 +0530

    Practice

--- Pointer/Arithmatic Pointar/04.Pointar Arithmatic -Decrement By Integral Constant.c
+++ Pointer/Arithmatic Pointar/04.Pointar Arithmatic -Decrement By Integral Constant.c
@@ -8,17 +8,17 @@
 
      printf("\n Base Address Of Array = %d",Arr);
 
-     printf("\n Value Of Pointar = %d",ptr);
+     printf("\n Value Of Pointer = %d",ptr);
 
-     printf("\n Value Of Array Pointer Poninting = %d",*ptr);
+     printf("\n Value Of Array Pointer Pointing = %d",*ptr);
 
      getch();
 
      ptr = ptr- 2;
 
 
-     printf("\n\n New Value Pointar Is = %d",ptr);
-     printf("\n Value Of Array Pointar Pointing = %d",*ptr);
+     printf("\n\n New Value Pointer Is = %d",ptr);
+     printf("\n Value Of Array Pointer Pointing = %d",*ptr);
 
      getch();
      return 0;
