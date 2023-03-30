c90dc5ad59a45e4944538b1aa03367a76a05f0da
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-03-30 23:23:31 +0530

    Practice

--- Structure Program/Basic Program Of Structure/05B.Accepting Structure .c
+++ Structure Program/Basic Program Of Structure/05B.Accepting Structure .c
@@ -10,17 +10,17 @@
  {
      struct Bus_Stand Bus[3];
 
-     strcpy(Bus[0].Bus_Name,"PUNE");    //Bus Numbar One
+     strcpy(Bus[0].Bus_Name,"PUNE");    //Bus Number One
      Bus[0].Bus_Numbar=3333;
 
-     strcpy(Bus[1].Bus_Name,"KHOLAPUR");    //Bus Numbar Two
+     strcpy(Bus[1].Bus_Name,"KHOLAPUR");    //Bus Number Two
      Bus[1].Bus_Numbar=2343;
 
-     strcpy(Bus[2].Bus_Name,"MUMBAI");      //Bus Numbar Three
+     strcpy(Bus[2].Bus_Name,"MUMBAI");      //Bus Number Three
      Bus[2].Bus_Numbar=9876;
 
-     printf("\n*****Frist Numabr Bus*****\n\n Bus Name=%s \n Bus_Numbar =%d \n",Bus[0].Bus_Name,Bus[0].Bus_Numbar);
-     printf("\n*****Second Numbar Bus*****\n\n Bus Name=%s \n Bus_Numbar =%d \n",Bus[1].Bus_Name,Bus[1].Bus_Numbar);
-     printf("\n*****Third Numbar Bus******\n\n Bus Name=%s \n Bus_Numbar =%d \n",Bus[2].Bus_Name,Bus[2].Bus_Numbar);
+     printf("\n*****Frist Numabr Bus*****\n\n Bus Name=%s \n Bus_Number =%d \n",Bus[0].Bus_Name,Bus[0].Bus_Numbar);
+     printf("\n*****Second Numbar Bus*****\n\n Bus Name=%s \n Bus_Number =%d \n",Bus[1].Bus_Name,Bus[1].Bus_Numbar);
+     printf("\n*****Third Numbar Bus******\n\n Bus Name=%s \n Bus_Number =%d \n",Bus[2].Bus_Name,Bus[2].Bus_Numbar);
  }
 
