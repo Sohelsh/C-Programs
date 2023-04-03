24998702351f0876cbc1045776a13fa29ba27aab
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-03 23:14:39 +0530

    Practice

---
+++ Class_Programs/Without_Function/02_Fibonacci_Series.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No1 = 1,No2 = 0,No3 = 0,Cnt = 0;
+
+    printf("\n Enter Count = ");
+    scanf("%d",&Cnt);
+
+    while(Cnt > 0)
+    {
+        printf(" %d ",No3);
+        No3 = No1 + No2;
+        No1 = No2;
+        No2 = No3;
+
+        Cnt--;
+    }
+
+    getch();
+    return 0;
+
+}
+
+
