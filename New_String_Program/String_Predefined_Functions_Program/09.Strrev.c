26497467aacccaed66324328f031095e65939951
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-09 23:29:45 +0530

    Use Of Predefined Function

---
+++ New_String_Program/String_Predefined_Functions_Program/09.Strrev.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+int main()
+{
+        char Str[20] = "I Am From Delhi";
+
+        printf("\n Your String Is = %s",strrev(Str));
+
+        getch();
+        return 0;
+}
+
