dc0199d112aa15c400e92d6d96817e1baac76813
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-11 23:19:31 +0530

    String Program

---
+++ New_String_Program/String Program Inside Main/05.Capital_Latter_Count.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Str[20] = "";
+    int Cnt = 0, i = 0;
+
+    printf("\n Enter String = ");
+    gets(Str);
+
+    while(Str[i] != '\0')
+    {
+        if(Str[i] >='A' && Str[i] <='Z')
+        {
+            Cnt++;
+        }
+
+        i++;
+    }
+
+    printf("\n Capital Latter Count Is = %d",Cnt);
+
+    getch();
+    return 0;
+}
+
