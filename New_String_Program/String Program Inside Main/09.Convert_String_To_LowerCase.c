20cbf0c47f5c6a577203ae81c41bda3f33943983
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-11 23:20:44 +0530

    String Program

---
+++ New_String_Program/String Program Inside Main/09.Convert_String_To_LowerCase.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char Str[20] = "";
+    int i = 0;
+
+    printf("\n Enter String  = ");
+    gets(Str);
+
+    while(Str[i] != '\0')
+    {
+        if(Str[i] >= 'A' && Str[i] <= 'Z')
+        {
+            Str[i] += 32;
+        }
+        i++;
+    }
+
+    printf("\n Your String Is = %s",Str);
+
+    getch();
+    return 0;
+}
+
