09c5303a733f5d337ab9a872932e995469a3dc3b
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-14 23:57:15 +0530

    Practice

---
+++ Practise Problem Of C programing/New Pattern Program/04B_Star_Left_Upper_Triangle.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R = 0, C = 0;
+    int i = 0,j = 0;
+
+    printf("\n Enter Row = ");
+    scanf("%d",&R);
+
+    printf("\n Enter Column = ");
+    scanf("%d",&C);
+
+    for(i = 1;i <= R;i++)
+    {
+        for(j = 1;j <= C - i + 1;j++)
+        {
+            printf(" * ");
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
