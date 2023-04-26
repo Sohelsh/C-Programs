1d1c572d7b4402a269cc52a182c502f563f11e7d
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-26 23:06:39 +0530

    Pattern

--- Practise Problem Of C programing/New Pattern Program/03.Star_Triangle.c
+++ Practise Problem Of C programing/New Pattern Program/03.Star_Triangle.c
@@ -3,14 +3,11 @@
 
 int main()
 {
-    int R = 0, C = 0;
+    int R = 0;
     int i = 0,j = 0;
 
     printf("\n Enter Row = ");
     scanf("%d",&R);
-
-    printf("\n Enter Column = ");
-    scanf("%d",&C);
 
     for(i = 1;i <= R;i++)
     {
