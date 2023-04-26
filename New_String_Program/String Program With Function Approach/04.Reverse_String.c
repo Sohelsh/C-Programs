aea0caa8b739e0d7c07032d12488b205d97e6598
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-26 23:05:46 +0530

    String Program

--- New_String_Program/String Program With Function Approach/04.Reverse_String.c
+++ New_String_Program/String Program With Function Approach/04.Reverse_String.c
@@ -1,25 +1,24 @@
 #include<stdio.h>
 #include<conio.h>
 
-int Reverse_String(char *);
+int Reverse_String(char *,char*);
 
 int main()
 {
-    char Str[20] = "";
+    char Str[20] = "",N_Str[20] = "";;
 
     printf("\n Enter String = ");
     gets(Str);
 
-    printf("\n Reverse String Is = %s",Reverse_String(Str));
+    printf("\n Reverse String Is = %s",Reverse_String(Str,N_Str));
 
     getch();
     return 0;
 }
 
-int Reverse_String(char *Str1)
+int Reverse_String(char *Str1,char *N_Str)
 {
     int i = 0,j = 0;
-    char N_Str[20] = "";
 
     while(Str1[i] != '\0')
     {
@@ -34,8 +33,5 @@ int Reverse_String(char *Str1)
         i--;
         j++;
     }
-
-    printf("\n Reverse String Is = %s",N_Str);
-
     return N_Str;
 }
