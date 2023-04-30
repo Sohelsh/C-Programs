6a11a9542d70c6af75c21debe65accae6d58e587
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-30 23:02:07 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_1.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main() {
+    int A, B;
+    int C, D, E;
+    int F, G,H,I;
+
+    scanf("%d%d", &A , &B );
+    scanf("%d%d%d", &C, &D, &E );
+    scanf("%d%d%d%d", &F , &G, &H, &I );
+    printf("%d %d %d %d %d %d %d %d %d",A,B,C,D,E,F,G,H,I );
+
+    return 0;
+}
