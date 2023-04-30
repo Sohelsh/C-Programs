31d7eda8f26aabee817630fef1ea89e4590d2474
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-30 23:02:39 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+    int No = 5;
+
+    printf("%d\n",No);
+
+    char A[5] = "ABCD";
+
+    printf("%s\n",A);
+
+    int Arr[] = {1, 2,3,4};
+    int i = 0;
+    for(i = 0; i < 4;i++)
+    {
+        printf("%d",Arr[i]);
+    }
+
+    return 0;
+}
