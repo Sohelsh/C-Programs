a8d4eeea057a8c3e03896348297daebf0a5298ca
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-04-30 23:02:25 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    char A[10], B[10];
+    char C[10], D[10], E[10];
+    char F[10], G[10],H[10],I[10];
+
+    scanf("%s%s", A , B );
+    scanf("%s%s%s", C, D, E );
+    scanf("%s%s%s%s", F , G, H, I );
+    printf("%s %s %s %s %s %s %s %s %s  ", A,B,C,D,E,F,G,H,I);
+
+    return 0;
+}
