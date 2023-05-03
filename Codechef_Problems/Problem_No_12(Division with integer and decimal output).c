6f1fd5570c2b4418212a2b7ccde84c642075b6c8
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-03 20:51:31 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_12(Division with integer and decimal output).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int t,A,B;
+    float X;
+    int Y;
+    int i = 1;
+    scanf("%d", &t );
+    while (i <= t){
+        scanf("%d%d",&A,&B);
+        X = (float)A / B;
+        Y = A / B;
+        printf("%f %d\n",X ,Y);
+        i = i+1;
+    }
+    return 0;
+}
