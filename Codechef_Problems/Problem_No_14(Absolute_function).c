7c46f45cb2ab8d65347394fa2038df1becf98dea
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-03 20:52:07 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_14(Absolute_function).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main()
+{
+    int t = 0,i = 0;
+    scanf("%d", &t);
+
+    for (i = 0; i < t; i++) {
+        int A, B;
+        scanf("%d %d", &A, &B);
+        int diff1 = A - B;
+        int diff2 = B - A;
+        printf("%d %d %d %d\n", diff1, diff2, abs(diff1), abs(diff2));
+    }
+    return 0;
+}
