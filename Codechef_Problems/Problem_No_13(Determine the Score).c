056c3494cea29ef7dbda0b1c616e0970564617e8
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-03 20:51:51 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_13(Determine the Score).c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int X, N, points_per_testcase, score;
+        scanf("%d%d", &X, &N);
+
+        points_per_testcase = X / 10;
+        score = points_per_testcase * N;
+
+        printf("%d\n", score);
+    }
+
+    return 0;
+}
