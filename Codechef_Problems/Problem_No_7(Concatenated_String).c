c64e47319ee3ce7be374e596e2e3bff8f01b6873
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-05 23:02:24 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_7(Concatenated_String).c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<string.h>
+#define Size 200
+int main()
+{
+    char Str1[Size] = {},Str2[Size] = {};
+    int Cnt = 0,i = 0,Len = 0,j = 0;
+
+    printf("\n Enter Count Of String = ");
+    scanf("%d",&Cnt);
+
+    for(i = 0; i < Cnt;i++)
+    {
+        printf("\n Enter First String = ");
+        scanf("%s",Str1);
+
+        printf("\n Enter String = ");
+        scanf("%s",Str2);
+
+        Len = strlen(Str1);
+
+        Str1[Len] = ' '; /// For White Space
+        Len = Len + 1;
+        for(j = 0;j <= Len;j++)
+        {
+            Str1[Len+j] = Str2[j];
+        }
+
+        Str1[Len+j] = '\0';
+
+        printf("\n\a Concatenated String Is = %s",Str1);
+
+    }
+
+    return 0;
+}
