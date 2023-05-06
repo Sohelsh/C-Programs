fd93f877729a71e11ed950e32b47197b7423fbda
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-06 23:03:49 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_19.c
@@ -0,0 +1,45 @@
+int main()
+{
+    int X = 3;
+    int Y = 4;
+    int Z = 12;
+
+        if ((Z%X == 0) && (Z%Y == 0))
+        {
+            printf("Z is divisible by both X and Y\n");
+        }
+        else if (Z%X == 0)
+        {
+            printf("Z is divisible only by X\n");
+        }
+        else if (Z%Y == 0)
+        {
+            printf("Z is divisible only by Y\n");
+        }
+        else
+        {
+            printf("Z is divisible by neither X nor Y\n");
+        }
+
+        X = 3;
+        Y = 5;
+        Z = 13;
+        if ((Z%X == 0) && (Z%Y == 0))
+        {
+            printf("Z is divisible by both X and Y\n");
+        }
+        else if (Z%X == 0)
+        {
+            printf("Z is divisible only by X\n");
+        }
+        else if (Z%Y == 0)
+        {
+            printf("Z is divisible only by Y\n");
+        }
+        else
+        {
+            printf("Z is divisible by neither X nor Y\n");
+        }
+
+    return 0;
+}
