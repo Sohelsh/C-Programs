e7e969ce8fdf5378971396286aea8114670973a4
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-07 23:55:38 +0530

    Pratice

---
+++ Codechef_Problems/Problem_No_24.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#define N  4
+int main()
+{
+    int A[N] = {0};
+    int i = 0,j = 0;
+
+    for(i = 0; i < N;i++)
+    {
+       scanf("%d",&A[i]);
+    }
+
+     for(i = 0; i < N;i++)
+    {
+        printf("%d ",A[i]);
+    }
+
+    printf("\n");
+
+    for(j = i - 1; j >= 0;j--)
+    {
+        printf("%d ",A[j]);
+    }
+
+    getch();
+    return 0;
+
+}
