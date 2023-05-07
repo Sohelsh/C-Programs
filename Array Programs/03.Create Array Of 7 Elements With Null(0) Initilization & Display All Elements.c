adb01647d3710611dc9555160dc93a23c576b283
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-07 23:56:26 +0530

    Array Programs

--- Array Programs/03.Create Array Of 7 Elements With Null(0) Initilization & Display All Elements.c
+++ Array Programs/03.Create Array Of 7 Elements With Null(0) Initilization & Display All Elements.c
@@ -5,13 +5,13 @@
  {
            int Value[7]= {0};
 
-           printf("\n Frist   Element = %d",Value[0]);
+           printf("\n First   Element = %d",Value[0]);
            printf("\n Second  Element = %d",Value[1]);
            printf("\n Third   Element = %d",Value[2]);
            printf("\n Fourth  Element = %d",Value[3]);
            printf("\n Fifth   Element = %d",Value[4]);
            printf("\n Sixth   Element = %d",Value[5]);
-           printf("\n Sevnth  Element = %d",Value[6]);
+           printf("\n Seventh  Element = %d",Value[6]);
 
            getch();
            return 0;
