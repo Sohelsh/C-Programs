a86f243c06c9913942a9ec89ab1777e48419b858
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-10 21:19:20 +0530

    Practice

--- Codechef_Problems/Problem_No_4.c
+++ Codechef_Problems/Problem_No_4.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 #include<conio.h>
 
-int main() {
-    int t;
-    int A,B;
-    char C[30];
-    int i = 1;
+int main()
+ {
+        int t;
+        int A,B;
+        char C[30];
+        int i = 1;
 
-    scanf("%d", &t );
+        scanf("%d", &t );
 
-    while( i <= t)
-    {
-        scanf("%d%d", &A, &B );
-        scanf("%s", &C);
-        printf("%d %d %s \n ", A,B,C );
-        i = i+i;
-    }
+        while( i <= t)
+        {
+            scanf("%d%d", &A, &B );
+            scanf("%s", &C);
+            printf("%d %d %s \n ", A,B,C );
+            i = i+i;
+        }
 
-    getch();
-    return 0;
+        getch();
+        return 0;
 }
