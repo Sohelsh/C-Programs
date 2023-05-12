e5444d9f0a9b60fc711484b7d79f0d6671556587
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-12 22:13:23 +0530

    Practice

--- Codechef_Problems/Problem_No_10.c
+++ Codechef_Problems/Problem_No_10.c
@@ -1,14 +1,19 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
-int main() {
+int main()
+ {
     int t,X;
     int i = 1;
     scanf("%d", &t );
-    while (i <= t){
+    while (i <= t)
+    {
         scanf("%d", &X);
         X = X * 2;
         printf("\n%d",X*5);
         i = i+1;
     }
+
+    getch();
     return 0;
 }
