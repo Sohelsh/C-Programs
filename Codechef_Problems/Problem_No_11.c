8241200475f2c7eb18fdb98763259246f17ccfae
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-12 22:14:07 +0530

    Practice

--- Codechef_Problems/Problem_No_11.c
+++ Codechef_Problems/Problem_No_11.c
@@ -1,13 +1,17 @@
 
 #include <stdio.h>
 
-int main() {
+int main()
+ {
     int t,A,B;
     int i = 1;
     int D,Q;
     scanf("%d", &t );
-    while (i <= t){
+    while (i <= t)
+    {
+        printf("\n Enter Number = ");
         scanf("%d%d",&A,&B);
+
         D = A - B;
         Q = A / B;
         printf("%d %d\n",D,Q);
