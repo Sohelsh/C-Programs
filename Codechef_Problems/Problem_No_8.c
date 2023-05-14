474017b25b0b13a63bb5e55a6b57707122eed0fd
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-14 23:19:28 +0530

    Practise

--- Codechef_Problems/Problem_No_8.c
+++ Codechef_Problems/Problem_No_8.c
@@ -6,9 +6,13 @@ int main()
     int t,A,B;
     int P,S;
     int i = 1;
+
+        printf("\n Enter Number :");
         scanf("%d", &t );
+
         while (i <= t)
         {
+            printf("\n Enter Number :");
             scanf("%d %d", &A,&B);
 
             S = A + B;
