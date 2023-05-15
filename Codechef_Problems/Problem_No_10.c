02ef0d36cc70f9eba8757ff16a60080978eddebf
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-15 22:51:08 +0530

    Practise

--- Codechef_Problems/Problem_No_10.c
+++ Codechef_Problems/Problem_No_10.c
@@ -3,10 +3,11 @@
 
 int main()
  {
-    int t,X;
+    int t = 0,X = 0;
     int i = 1;
+
       printf("\n Enter Number :");
-    scanf("%d", &t );
+      scanf("%d", &t );
     while (i <= t)
     {
           printf("\n Enter Number :");
