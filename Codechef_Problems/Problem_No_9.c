98812d51b723c3e55aa0270e9bd14a67bde3d53c
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-15 22:52:26 +0530

    Practise

--- Codechef_Problems/Problem_No_9.c
+++ Codechef_Problems/Problem_No_9.c
@@ -3,15 +3,17 @@
 
 int main()
 {
-    int t,X,Y;
+    int t = 0,X = 0,Y = 0;
     int i = 1;
 
     printf("\n Enter Number :");
     scanf("%d", &t );
+
     while (i <= t){
 
         printf("\n Enter Number :");
         scanf("%d %d", &X,&Y);
+
         printf("%d\n",X*Y);
         i = i+1;
 
