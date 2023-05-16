aad7c1a6ae617779d20053ffcfc14bec22437a45
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-16 23:40:22 +0530

    Practise

--- Codechef_Problems/Problem_No_12(Division with integer and decimal output).c
+++ Codechef_Problems/Problem_No_12(Division with integer and decimal output).c
@@ -1,11 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
-int main() {
+int main()
+{
     int t,A,B;
     float X;
     int Y;
     int i = 1;
+
+    printf("\n Enter Number = ");
     scanf("%d", &t );
+
     while (i <= t){
 
         printf("\n Enter Number = ");
@@ -15,5 +20,7 @@ int main() {
         printf("%f %d\n",X ,Y);
         i = i+1;
     }
+
+    getch();
     return 0;
 }
