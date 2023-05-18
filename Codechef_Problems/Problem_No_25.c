09617e1bd269d3ebed44fe7e6cfdc7fb1b5e9b4e
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-18 22:30:50 +0530

    Practise

--- Codechef_Problems/Problem_No_25.c
+++ Codechef_Problems/Problem_No_25.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
 
 int main()
 {
@@ -36,5 +37,6 @@ int main()
         printf("%d %d %d\n", pos, neg, divk);
     }
 
+    getch();
     return 0;
 }
