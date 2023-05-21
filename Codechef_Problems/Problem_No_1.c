6776045e967261af012c214f428ca9c61035ee59
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-21 23:15:34 +0530

    Practise

--- Codechef_Problems/Problem_No_1.c
+++ Codechef_Problems/Problem_No_1.c
@@ -1,15 +1,16 @@
 #include<conio.h>
-#include <stdio.h>
+#include<stdio.h>
 
-int main() {
-        int A, B;
+int main()
+{
+        int A = 0, B = 0;
         int C, D, E;
         int F, G,H,I;
 
         printf("\n Enter Number = ");
-        scanf("%d%d", &A , &B );
-        scanf("%d%d%d", &C, &D, &E );
-        scanf("%d%d%d%d", &F , &G, &H, &I
+        scanf("%d%d",&A,&B);
+        scanf("%d%d%d",&C,&D,&E);
+        scanf("%d%d%d%d",&F,&G,&H,&I);
 
         printf("%d %d %d %d %d %d %d %d %d",A,B,C,D,E,F,G,H,I );
 
