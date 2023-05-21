396e1d631e1f0c1bafcfbc19218d0d9b1eba98ad
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-21 23:18:55 +0530

    Practise

--- Codechef_Problems/Problem_No_5.c
+++ Codechef_Problems/Problem_No_5.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<conio.h>
 
 int main()
 {
-    int t;
-    int n;
-    int i = 1;
+        int t = 0;
+        int n;
+        int i = 1;
 
-    printf("\n Enter Number = ");
-    scanf("%d\n", &t );
+        printf("\n Enter Number = ");
+        scanf("%d\n", &t );
 
-    while ( i <= t)
-    {
-        scanf("%d", &n);
-        printf("%d\n", n+1 );
-        i = i+1;
-    }
+        while ( i <= t)
+        {
+            scanf("%d", &n);
+            printf("%d\n", n+1 );
+            i = i+1;
+        }
 
-    getch();
-    return 0;
+        getch();
+        return 0;
 }
 
