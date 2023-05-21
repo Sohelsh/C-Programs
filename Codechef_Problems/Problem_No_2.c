5415aaf57a03029afa99ecfd8333d6779e96997a
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-21 23:18:06 +0530

    Practise

--- Codechef_Problems/Problem_No_2.c
+++ Codechef_Problems/Problem_No_2.c
@@ -8,9 +8,9 @@ int main() {
         char F[10], G[10],H[10],I[10];
 
         printf("\n Enter Numbers = ");
-        scanf("%s%s", A , B );
-        scanf("%s%s%s", C, D, E );
-        scanf("%s%s%s%s", F , G, H, I );
+        scanf("%s%s",&A,&B);
+        scanf("%s%s%s",&C,&D,&E);
+        scanf("%s%s%s%s",&F,&G,&H,&I);
         printf("%s %s %s %s %s %s %s %s %s  ", A,B,C,D,E,F,G,H,I);
 
         getch();
