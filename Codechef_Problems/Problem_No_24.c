d15196da9ac3dfe8b6ddc9f17717a29250aa1dd4
Sohel Shaikh <92326388+Sohelsh@users.noreply.github.com>
2023-05-25 23:39:44 +0530

    D

--- Codechef_Problems/Problem_No_24.c
+++ Codechef_Problems/Problem_No_24.c
@@ -4,28 +4,28 @@
 
 int main()
 {
-    int A[N] = {0};
-    int i = 0,j = 0;
+        int A[N] = {0};
+        int i = 0,j = 0;
 
-    for(i = 0; i < N;i++)
-    {
-       printf("\n Enter Number = ");
-       scanf("%d",&A[i]);
-    }
+        for(i = 0; i < N;i++)
+        {
+           printf("\n Enter Number = ");
+           scanf("%d",&A[i]);
+        }
 
-     for(i = 0; i < N;i++)
-    {
-        printf("%d ",A[i]);
-    }
+         for(i = 0; i < N;i++)
+        {
+            printf("%d ",A[i]);
+        }
 
-    printf("\n");
+        printf("\n");
 
-    for(j = i - 1; j >= 0;j--)
-    {
-        printf("%d ",A[j]);
-    }
+        for(j = i - 1; j >= 0;j--)
+        {
+            printf("%d ",A[j]);
+        }
 
-    getch();
-    return 0;
+        getch();
+        return 0;
 
 }
