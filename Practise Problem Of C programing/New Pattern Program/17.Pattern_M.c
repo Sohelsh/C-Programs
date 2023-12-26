7838e699c360901007dc18f6e60c7713c4112977
Sohelsh <sohelshaikhadam@gmail.com>
2023-12-26 21:20:36 +0530

    Practice

---
+++ Practise Problem Of C programing/New Pattern Program/17.Pattern_M.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<conio.h>         ///Pending Program
+
+int main()
+{
+    int R = 0;
+
+    int i = 0,j = 0;
+
+    printf("\n Enter Row = ");
+    scanf("%d",&R);
+
+    for(i = 1;i <= R;i++)
+    {
+        for(j = 1;j <= R;j++)
+        {
+
+            if(j == 1|| j == R ||(i == j) || i == (R/ 2 + 1))
+            {
+                 printf("* ");
+            }
+            else
+            {
+                 printf("  ");
+            }
+
+        }
+        printf("\n");
+    }
+
+    getch();
+    return 0;
+}
+
+
+
+
+
